ac3c7b93d59616bddecddf5be700bf5f43bdc2c3
LoganMMcDonald <logan.mcdonald@gsa.gov>
2022-08-04 09:41:02 -0400

    add error catching for whois command

--- docs/research/scripts/icann_lookup.py
+++ docs/research/scripts/icann_lookup.py
@@ -13,18 +13,22 @@ GOV_URLS_CSV_URL = "https://raw.githubusercontent.com/GSA/govt-urls/master/1_gov
 
 data = requests.get(GOV_URLS_CSV_URL).text
 csv_data = list(csv.reader(data.splitlines(), delimiter=','))
-domains = csv_data[1:5]
-fields = csv_data[0] + ['registrar']
+domains = csv_data[1:]
+fields = csv_data[0] + ['Registrar']
 
 def check_registration(name):
-    domain_info = whois.whois(name)
-    return domain_info['Registrar']
+    try:
+        domain_info = whois.whois(name)
+        return domain_info['registrar']
+    except:
+        print('Something went wrong')
 
 full_data = []
 for domain in domains:
-    domain_name = domain[0]
-    registrar = check_registration(domain_name)
-    full_data.append(domain + [registrar])
+    domain_name = domain[0].lower()
+    if domain_name.endswith('.com') or domain_name.endswith('.edu') or domain_name.endswith('.net'):
+        registrar = check_registration(domain_name)
+        full_data.append(domain + [registrar])
 
 with open('../data/registrar_data.csv', 'w') as f:
     writer = csv.writer(f)
