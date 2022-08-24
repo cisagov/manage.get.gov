8826c3b75dd34570cd58eaf28c50a2f7568c7750
LoganMMcDonald <logan.mcdonald@gsa.gov>
2022-08-24 08:33:40 -0400

    make mypy run successfully, add configuration files rather than specifying in ci

--- src/registrar/config/settings.py
+++ src/registrar/config/settings.py
@@ -100,8 +100,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
-            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] "
-            "%(message)s",
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] " "%(message)s",
             "datefmt": "%d/%b/%Y %H:%M:%S",
         },
         "simple": {
