10be23727da1ff7ceb69f606939c44bd5448c61d
LoganMMcDonald <logan.mcdonald@gsa.gov>
2022-08-26 12:23:30 -0400

    fix issue maybe

--- src/registrar/config/settings.py
+++ src/registrar/config/settings.py
@@ -100,7 +100,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
-            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] " "%(message)s",
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] " "%(message)s", # noqa
             "datefmt": "%d/%b/%Y %H:%M:%S",
         },
         "simple": {
