78d1365e53ec99ffc2c7ce50e21057f8b8ceedf4
igorkorenfeld <igor.korenfeld@gsa.gov>
2022-09-08 15:32:28 -0400

    Setup gulpfile directories and exports

---
+++ src/gulpfile.js
@@ -0,0 +1,35 @@
+/* gulpfile.js */
+
+const uswds = require('@uswds/compile');
+
+/**
+ * USWDS version
+ * Set the version of USWDS you're using (2 or 3)
+ */
+
+uswds.settings.version = 3;
+
+/**
+ * Path settings
+ * Set as many as you need
+ */
+
+const ASSETS_DIR = './registrar/assets/';
+
+uswds.paths.dist.css = ASSETS_DIR + 'css';
+uswds.paths.dist.sass = ASSETS_DIR + 'sass';
+uswds.paths.dist.theme = ASSETS_DIR + 'sass/_theme';
+uswds.paths.dist.fonts = ASSETS_DIR + 'fonts';
+uswds.paths.dist.js = ASSETS_DIR + 'js';
+uswds.paths.dist.img = ASSETS_DIR + 'img';
+
+/**
+ * Exports
+ * Add as many as you need
+ */
+
+exports.init = uswds.init;
+exports.compile = uswds.compile;
+exports.watch = uswds.watch;
+exports.copyAssets = uswds.copyAssets
+                                                                                  
