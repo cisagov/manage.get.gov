65bb21ff0f961d694d366fe4f0082d66302dac98
igorkorenfeld <igor.korenfeld@gsa.gov>
2022-09-08 15:37:43 -0400

    Add templates, styles, views for testing USWDS setup

--- src/registrar/config/settings.py
+++ src/registrar/config/settings.py
@@ -128,7 +128,11 @@ WSGI_APPLICATION = "registrar.config.wsgi.application"
 # will place static files for deployment.
 # Do not use this directory for permanent storage -
 # it is for Django!
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "public"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+]
 
 # TODO: decide on template engine and document in ADR
 TEMPLATES = [
