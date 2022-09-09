96d191b7259994648ee6662dbabf8409b88a7e04
igorkorenfeld <igor.korenfeld@gsa.gov>
2022-09-09 16:32:48 -0400

    Move manifest command to run script, add collectstatic

---
+++ src/run.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+set -o errexit
+set -o pipefail
+
+# Only run migrations on the zeroth index when in a cloud.gov environment
+if [[ -v CF_INSTANCE_INDEX && $CF_INSTANCE_INDEX == 0 ]]
+then
+  python manage.py migrate --settings=registrar.config.settings --noinput
+else
+  echo "Migrations did not run."
+  if [[ -v CF_INSTANCE_INDEX ]]
+  then
+    echo "CF Instance Index is ${CF_INSTANCE_INDEX}."
+  fi
+fi
+
+python manage.py collectstatic --settings=registrar.config.settings --noinput
+gunicorn registrar.config.wsgi -t 60
