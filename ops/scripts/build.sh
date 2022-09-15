2f09db0ec55a2c594170c3752838f9318ae556f3
igorkorenfeld <igor.korenfeld@gsa.gov>
2022-09-15 15:13:04 -0400

    Add a build script for prearing assets for deploy

---
+++ ops/scripts/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Compile assets
+docker compose run node npx gulp compile;
+docker compose run node npx gulp copyAssets;
+
+# Collect assets
+docker compose run app python manage.py collectstatic --noinput
