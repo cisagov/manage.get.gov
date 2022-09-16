0271574e681b9863261ec6662d8ea5b8c285203e
igorkorenfeld <igor.korenfeld@gsa.gov>
2022-09-16 12:43:40 -0400

    Add comment about running collectstatic as breadcrumb in run script

--- src/run.sh
+++ src/run.sh
@@ -15,4 +15,7 @@ else
   fi
 fi
 
+# Make sure that django's `collectstatic` has been run locally before pushing up to unstable,
+# so that the styles and static assets to show up correctly on unstable.
+
 gunicorn registrar.config.wsgi -t 60
