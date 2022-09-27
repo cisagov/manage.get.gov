c5f5b3ce31a5fbae3d969f0a68503180554b0ecc
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2022-09-27 13:23:41 -0500

    Fix signal handling with correct User class

--- src/registrar/signals.py
+++ src/registrar/signals.py
@@ -1,8 +1,7 @@
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
 from django.dispatch import receiver
 
-from .models import UserProfile
+from .models import User, UserProfile
 
 
 @receiver(post_save, sender=User)
