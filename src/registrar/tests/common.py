021c8fee2481e732096de4e6c6904fc1ac2311ec
Seamus Johnston <seamus.johnston@gsa.gov>
2022-10-07 10:40:47 -0500

    Add logged in scanning to CI

---
+++ src/registrar/tests/common.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model, login
+
+class MockUserLogin:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_anonymous:
+            user = None
+            UserModel = get_user_model()
+            username = "Testy"
+            args = {
+                UserModel.USERNAME_FIELD: username,
+            }
+            try:
+                user = UserModel.objects.get(**args)
+            except UserModel.DoesNotExist:
+                user = UserModel(**args)
+                user.is_staff = True
+                user.is_superuser = True
+                user.save()
+            if user is not None:
+                backend = settings.AUTHENTICATION_BACKENDS[-1]
+                login(request, user, backend=backend)
+
+        response = self.get_response(request)
+        return response
