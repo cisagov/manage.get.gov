408e5f09a8cab64601cef776b578f4f1d87d9a96
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2022-10-07 11:28:44 -0500

    Fix security check tests in Actions environment

--- src/registrar/config/settings.py
+++ src/registrar/config/settings.py
@@ -230,9 +230,7 @@ SERVER_EMAIL = "root@get.gov"
 
 # Content-Security-Policy configuration
 # this can be restrictive because we have few external scripts
-allowed_sources = (
-	"'self'",
-)
+allowed_sources = ("'self'",)
 CSP_DEFAULT_SRC = allowed_sources
 # Most things fall back to default-src, but these two do not and should be
 # explicitly set
