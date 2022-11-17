523c699865602ff2fb47396f12886f5e15f9e222
Seamus Johnston <seamus.johnston@gsa.gov>
2022-11-17 07:43:53 -0600

    Make tests a little less noisy

--- src/api/tests/common.py
+++ src/api/tests/common.py
@@ -3,9 +3,6 @@ import logging
 
 from contextlib import contextmanager
 
-from django.conf import settings
-from django.contrib.auth import get_user_model, login
-
 
 def get_handlers():
     """Obtain pointers to all StreamHandlers."""
@@ -50,28 +47,3 @@ def less_console_noise():
         # restore the streams
         for handler in handlers.values():
             handler.setStream(restore[handler.name])
-
-
-class MockUserLogin:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        if request.user.is_anonymous:
-            user = None
-            UserModel = get_user_model()
-            username = "Testy"
-            args = {
-                UserModel.USERNAME_FIELD: username,
-            }
-            user, _ = UserModel.objects.get_or_create(**args)
-            user.is_staff = True
-            user.is_superuser = True
-            user.save()
-            backend = settings.AUTHENTICATION_BACKENDS[-1]
-            login(request, user, backend=backend)
-
-        response = self.get_response(request)
-        return response
-
-
