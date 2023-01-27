69533a2f80cdaa7a908d81788702aeb08ed2fea5
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2023-01-27 11:51:30 -0600

    Sortable table and dashboard_view styling

--- src/registrar/views/index.py
+++ src/registrar/views/index.py
@@ -7,6 +7,8 @@ def index(request):
     """This page is available to anyone without logging in."""
     context = {}
     if request.user.is_authenticated:
+        # the logged-in index view IS the dashboard
+        context["dashboard_view"] = True
         applications = DomainApplication.objects.filter(creator=request.user)
         context["domain_applications"] = applications
     return render(request, "home.html", context)
