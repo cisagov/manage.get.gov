ecaa870005f0b2e933e34bd7d3c90180a1be3594
Jon Roberts <jon.roberts@gsa.gov>
2023-02-27 10:54:36 -0700

    Cleaning up a little more

--- src/registrar/forms/application_wizard.py
+++ src/registrar/forms/application_wizard.py
@@ -28,10 +28,8 @@ class RegistrarForm(forms.Form):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("label_suffix", "")
         # save a reference to an application object
-        archived = kwargs.pop('archived', None)
         self.application = kwargs.pop("application", None)
         super(RegistrarForm, self).__init__(*args, **kwargs)
-        self.fields['archived'] = forms.BooleanField(initial=archived, required=False)
 
     def to_database(self, obj: DomainApplication | Contact):
         """
