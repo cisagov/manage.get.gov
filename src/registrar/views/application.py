bebc00cee8817a649b338d2da683026ace88b98a
Jon Roberts <jon.roberts@gsa.gov>
2023-04-19 14:18:28 -0600

    adding login mixin along wiht domainpermission

--- src/registrar/views/application.py
+++ src/registrar/views/application.py
@@ -488,7 +488,7 @@ class ApplicationStatus(generic.DetailView):
         return context
 
 
-class ApplicationWithdraw(generic.DetailView, DomainPermission):
+class ApplicationWithdraw(generic.DetailView, DomainPermission, LoginRequiredMixin):
     model = DomainApplication
     template_name = "application_withdraw_confirmation.html"
     """ The page above will display asking user to confirm if they want to withdraw;
