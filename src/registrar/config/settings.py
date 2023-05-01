ef36d50925316d8f3941d3e98becef2511550c1b
LoganMMcDonald <logan.mcdonald@gsa.gov>
2023-05-01 13:49:01 -0700

    Add new developer sandbox 'rjm' infrastructure

--- src/registrar/config/settings.py
+++ src/registrar/config/settings.py
@@ -549,6 +549,7 @@ SECURE_SSL_REDIRECT = True
 # web server configurations.
 ALLOWED_HOSTS = [
     "getgov-stable.app.cloud.gov",
+    "getgov-rjm.app.cloud.gov",
     "getgov-jon.app.cloud.gov",
     "getgov-mr.app.cloud.gov",
     "getgov-sspj.app.cloud.gov",
