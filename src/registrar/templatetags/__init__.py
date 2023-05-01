56df44643e8e7445f5e68b05612e3419c746783b
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2023-05-01 15:44:20 -0500

    Add sublabels and nonfunctional add-another button

--- src/registrar/templatetags/__init__.py
+++ src/registrar/templatetags/__init__.py
@@ -6,3 +6,8 @@ from django.template.defaulttags import register
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def concat(arg1, arg2):
+    """concatenate arg1 & arg2"""
+    return str(arg1) + str(arg2)
