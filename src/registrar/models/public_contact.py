c434dff9a5efa05bb384d46ede64f7d550dbbebe
Seamus Johnston <seamus.johnston@gsa.gov>
2023-05-02 09:59:26 -0500

    Update help text for Public Contact

--- src/registrar/models/public_contact.py
+++ src/registrar/models/public_contact.py
@@ -14,37 +14,79 @@ class PublicContact(TimeStampedModel):
         TECHNICAL = "technical", "Technical"
         SECURITY = "security", "Security"
 
-    contact_type = models.CharField(max_length=14, choices=ContactTypeChoices.choices)
+    contact_type = models.CharField(
+        max_length=14,
+        choices=ContactTypeChoices.choices,
+        help_text="For which type of WHOIS contact"
+    )
 
-    # contact's full name
-    name = models.TextField(null=False)
-    # contact's organization (null ok)
-    org = models.TextField(null=True)
-    # contact's street
-    street1 = models.TextField(null=False)
-    # contact's street (null ok)
-    street2 = models.TextField(null=True)
-    # contact's street (null ok)
-    street3 = models.TextField(null=True)
-    # contact's city
-    city = models.TextField(null=False)
-    # contact's state or province
-    sp = models.TextField(null=False)
-    # contact's postal code
-    pc = models.TextField(null=False)
-    # contact's country code
-    cc = models.TextField(null=False)
-    # contact's email address
-    email = models.TextField(null=False)
-    # contact's phone number
-    # Must be in ITU.E164.2005 format
-    voice = models.TextField(null=False)
-    # contact's fax number (null ok)
-    # Must be in ITU.E164.2005 format
-    fax = models.TextField(null=True)
-    # contact's authorization code
-    # 16 characters minium
-    pw = models.TextField(null=False)
+    uuid = models.TextField(
+        null=True,
+        blank=True,
+        help_text="ID assigned by the registry"
+    )
+    has_edits = models.BooleanField(
+        default=True,
+        help_text="Whether edits have been made since last published"
+    )
+    domain = models.ForeignKey(
+        "registrar.Domain",
+        on_delete=models.PROTECT,
+        related_name="contacts",
+    )
+
+    name = models.TextField(
+        null=False,
+        help_text="Contact's full name"
+    )
+    org = models.TextField(
+        null=True,
+        help_text="Contact's organization (null ok)"
+    )
+    street1 = models.TextField(
+        null=False,
+        help_text="Contact's street"
+    )
+    street2 = models.TextField(
+        null=True,
+        help_text="Contact's street (null ok)"
+    )
+    street3 = models.TextField(
+        null=True,
+        help_text="Contact's street (null ok)"
+    )
+    city = models.TextField(
+        null=False,
+        help_text="Contact's city"
+    )
+    sp = models.TextField(
+        null=False,
+        help_text="Contact's state or province"
+    )
+    pc = models.TextField(
+        null=False,
+        help_text="Contact's postal code"
+    )
+    cc = models.TextField(
+        null=False,
+        help_text="Contact's country code"
+    )
+    email = models.TextField(
+        null=False,
+        help_text="Contact's email address"
+    )
+    voice = models.TextField(
+        null=False,
+        help_text="Contact's phone number. Must be in ITU.E164.2005 format"
+    )
+    fax = models.TextField(
+        null=True,
+        help_text="Contact's fax number (null ok). Must be in ITU.E164.2005 format."
+    )
+    pw = models.TextField(
+        null=False,
+        help_text="Contact's authorization code. 16 characters minimum."
+    )
 
     def __str__(self):
         return f"{self.name} <{self.email}>"
