400232a10f51a72528ac8d44b2ec428ab1e9a4b4
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2023-05-17 13:59:34 -0500

    Add public_site_url template tag

--- src/registrar/templatetags/url_helpers.py
+++ src/registrar/templatetags/url_helpers.py
@@ -1,5 +1,9 @@
+from urllib.parse import urljoin
+
 from django import template
 from django.urls import reverse
+
+from django.conf import settings
 
 register = template.Library()
 
@@ -15,3 +19,15 @@ def startswith(text, starts):
     if isinstance(text, str):
         return text.startswith(starts)
     return False
+
+
+@register.simple_tag
+def public_site_url(url_path):
+    """Make a full URL for this path at our public site.
+
+    The public site base url is set by a GETGOV_PUBLIC_SITE_URL environment
+    variable.
+    """
+    base_url = settings.GETGOV_PUBLIC_SITE_URL
+    public_url = urljoin(base_url, url_path)
+    return public_url
