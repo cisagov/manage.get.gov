2e7644bdc3fc8b477c54e64a2832f30079cb319d
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2023-06-01 16:05:15 -0500

    fix linting errors

--- src/registrar/views/health.py
+++ src/registrar/views/health.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from login_required import login_not_required
 
+
 # the health check endpoint needs to be globally available so that the
 # PaaS orchestrator can make sure the app has come up properly
 @login_not_required
