045ddbdd92d9d4467071f45e5ce1d4753936b83c
igorkorenfeld <igor.korenfeld@gsa.gov>
2023-06-09 11:46:53 -0400

    PR edit: additional test of the form

--- src/registrar/tests/test_views.py
+++ src/registrar/tests/test_views.py
@@ -1334,6 +1334,25 @@ class TestDomainDetail(TestWithDomainPermissions, WebTest):
         )
         self.assertContains(page, "Town of Igorville")
 
+    def test_domain_org_name_address_form(self):
+        """Submitting changes works on the org name address page."""
+        self.domain_information.organization_name = "Town of Igorville"
+        self.domain_information.save()
+        org_name_page = self.app.get(
+            reverse("domain-org-name-address", kwargs={"pk": self.domain.id})
+        )
+        session_id = self.app.cookies[settings.SESSION_COOKIE_NAME]
+
+        org_name_page.form["organization_name"] = "Not igorville"
+        org_name_page.form["city"] = "Faketown"
+
+        self.app.set_cookie(settings.SESSION_COOKIE_NAME, session_id)
+        success_result_page = org_name_page.form.submit()
+        self.assertEqual(success_result_page.status_code, 200)
+
+        self.assertContains(success_result_page, "Not igorville")
+        self.assertContains(success_result_page, "Faketown")
+
     def test_domain_your_contact_information(self):
         """Can load domain's your contact information page."""
         page = self.client.get(
