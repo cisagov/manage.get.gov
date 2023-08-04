87d717a1cf7a384a5ac5ea68e60d5fe626215106
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2023-08-04 13:37:30 -0600

    Readded index.py
    Status is not being removed from both tables so we need application_status in the context

--- src/registrar/views/index.py
+++ src/registrar/views/index.py
@@ -16,6 +16,7 @@ def index(request):
             pk=F("domain__id"),
             name=F("domain__name"),
             created_time=F("domain__created_at"),
+            application_status=F("domain__domain_application__status"),
         )
         context["domains"] = domains
     return render(request, "home.html", context)
