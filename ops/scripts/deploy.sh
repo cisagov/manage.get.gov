279a986ffdb03b8b9513d96f85e86ed9f96b7e72
rachidatecs <rachid.mrad@ecstech.com>
2023-08-21 16:34:43 -0400

    Add dk yaml, add dk to workflows, update org name

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -4,7 +4,7 @@
 ../ops/scripts/build.sh
 
 # Deploy to sandbox 
-cf target -o cisa-getgov-prototyping -s $1
+cf target -o cisa-dotgov -s $1
 cf push getgov-$1 -f ../ops/manifests/manifest-$1.yaml
 
 # migrations need to be run manually. Developers can use this command
