56084920b995b904c4a5823d6cf04822e9e1c8c6
rachidatecs <rachid.mrad@ecstech.com>
2023-08-28 17:32:18 -0400

    Add Katherine as analyst

--- src/registrar/fixtures.py
+++ src/registrar/fixtures.py
@@ -111,6 +111,12 @@ class UserFixture:
             "first_name": "David-Analyst",
             "last_name": "Kennedy-Analyst",
         },
+        {
+            "username": "91a9b97c-bd0a-458d-9823-babfde7ebf44",
+            "first_name": "Katherine-Analyst",
+            "last_name": "Osos-Analyst",
+            "email": "kosos@truss.works",
+        },
     ]
 
     STAFF_PERMISSIONS = [
