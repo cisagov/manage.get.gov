72a2e989844ff6ba81e13870085007dc29bc2ff2
Rachid Mrad <rachid.mrad@ecstech.com>
2023-09-01 14:51:58 -0400

    lint

--- src/registrar/signals.py
+++ src/registrar/signals.py
@@ -1,7 +1,6 @@
 import logging
 
-from django.core.management import call_command
-from django.db.models.signals import post_save, post_migrate
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import User, Contact
