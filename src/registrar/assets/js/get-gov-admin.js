48e56724acbe5ccb544d2b776c4142818b9b0aae
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2023-09-06 10:00:37 -0600

    Added comments on get-gov-admin.js

--- src/registrar/assets/js/get-gov-admin.js
+++ src/registrar/assets/js/get-gov-admin.js
@@ -28,7 +28,15 @@ function openInNewTab(el, removeAttribute = false){
  * but this can be expanded.
 */
 (function (){
-    // Keep scope tight and allow for scalability
+    /*
+    On mouseover, appends target="_blank" on domain_form under the Domain page.
+    The reason for this is that the template has a form that contains multiple buttons.
+    The structure of that template complicates seperating those buttons 
+    out of the form (while maintaining the same position on the page).
+    However, if we want to open one of those submit actions to a new tab - 
+    such as the manage domain button - we need to dynamically append target.
+    As there is no built-in django method which handles this, we do it here. 
+    */
     function prepareDjangoAdmin() {
         let domainFormElement = document.getElementById("domain_form");
         let domainSubmitButton = document.getElementById("manageDomainSubmitButton");
