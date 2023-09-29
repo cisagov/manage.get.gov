128f619e14f33ccb71dad619f93b9a9633b7e281
Rachid Mrad <rachid.mrad@ecstech.com>
2023-09-29 18:25:47 -0400

    revert permissions tests in views and admin for is_staff

--- src/registrar/admin.py
+++ src/registrar/admin.py
@@ -837,11 +837,12 @@ class DomainAdmin(ListHeaderAdmin):
         # Fixes a bug wherein users which are only is_staff
         # can access 'change' when GET,
         # but cannot access this page when it is a request of type POST.
-        if request.user.has_perm(
-            "registrar.full_access_permission"
-        ) or request.user.has_perm(
-            "registrar.analyst_access_permission"
-        ) or request.user.is_staff:
+        # if request.user.has_perm(
+        #     "registrar.full_access_permission"
+        # ) or request.user.has_perm(
+        #     "registrar.analyst_access_permission"
+        # ):
+        if request.user.is_staff:
             return True
         return super().has_change_permission(request, obj)
 
--- src/registrar/views/utility/mixins.py
+++ src/registrar/views/utility/mixins.py
@@ -63,9 +63,11 @@ class DomainPermission(PermissionsLoginMixin):
         """
 
         # Check if the user is permissioned...
-        user_is_analyst_or_superuser = self.request.user.has_perm(
-            "registrar.analyst_access_permission"
-        ) or self.request.user.has_perm("registrar.full_access_permission")
+        # user_is_analyst_or_superuser = self.request.user.has_perm(
+        #     "registrar.analyst_access_permission"
+        # ) or self.request.user.has_perm("registrar.full_access_permission")
+        
+        user_is_analyst_or_superuser = self.request.user.is_staff
 
         if not user_is_analyst_or_superuser:
             return False
