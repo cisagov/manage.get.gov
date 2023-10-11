67dacffa9f3bdf51dd51c040da944d5f53ec90b9
Erin <121973038+erinysong@users.noreply.github.com>
2023-10-11 12:01:06 -0700

    Update 0037_create_groups_v01 migration documentation

--- src/registrar/migrations/0037_create_groups_v01.py
+++ src/registrar/migrations/0037_create_groups_v01.py
@@ -2,11 +2,13 @@
 # It is dependent on 0035 (which populates ContentType and Permissions)
 # If permissions on the groups need changing, edit CISA_ANALYST_GROUP_PERMISSIONS
 # in the user_group model then:
+# [NOT RECOMMENDED]
 # step 1: docker-compose exec app ./manage.py migrate --fake registrar 0035_contenttypes_permissions
 # step 2: docker-compose exec app ./manage.py migrate registrar 0036_create_groups
 # step 3: fake run the latest migration in the migrations list
+# [RECOMMENDED]
 # Alternatively:
-# Only step: duplicate the migtation that loads data and run: docker-compose exec app ./manage.py migrate
+# Only step: duplicate the migration that loads data and run: docker-compose exec app ./manage.py migrate
 
 from django.db import migrations
 from registrar.models import UserGroup
