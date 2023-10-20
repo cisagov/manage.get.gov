070631b2160200850be76cd932af996aed677de6
Rebecca Hsieh <rebecca.hsieh@truss.works>
2023-10-20 10:19:57 -0700

    Fix naming for migration

--- src/registrar/migrations/0041_create_groups_v03.py
+++ src/registrar/migrations/0041_create_groups_v03.py
@@ -25,7 +25,7 @@ def create_groups(apps, schema_editor) -> Any:
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("registrar", "0040_alter_user_domainrole_role"),
+        ("registrar", "0040_alter_userdomainrole_role"),
     ]
 
     operations = [
