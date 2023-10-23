c713b8958f0ce0585f969d0530b080a4e0bab609
CocoByte <nicolle.leclair@gmail.com>
2023-10-23 14:54:02 -0600

    added test_domain_migration script and terminal_helper.  Still needs work
    Signed-off-by: CocoByte <nicolle.leclair@gmail.com>

--- src/registrar/management/commands/test_domain_migration.py
+++ src/registrar/management/commands/test_domain_migration.py
@@ -9,59 +9,76 @@ from django.core.management import BaseCommand
 from registrar.models import TransitionDomain
 from registrar.models import Domain
 from registrar.models import DomainInvitation
+from registrar.models.domain_information import DomainInformation
+
+from registrar.management.commands.utility.terminal_helper import TerminalColors
+from registrar.management.commands.utility.terminal_helper import TerminalHelper
+
+from registrar.management.commands.load_transition_domain import Command as load_transition_domain_command
 
 logger = logging.getLogger(__name__)
-
-
-class termColors:
-    """Colors for terminal outputs
-    (makes reading the logs WAY easier)"""
-
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    YELLOW = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
-    BackgroundLightYellow = "\033[103m"
-
 
 class Command(BaseCommand):
     help = """ """
 
     def add_arguments(self, parser):
+        """
+        OPTIONAL ARGUMENTS:
+        --debug
+        A boolean (default to true), which activates additional print statements
+        """
+
+        parser.add_argument("--runLoaders",
+            help="Runs all scripts (in sequence) for transition domain migrations",
+            action=argparse.BooleanOptionalAction)
+
+        # The file arguments have default values for running in the sandbox
+        parser.add_argument(
+            "--loaderDirectory",
+            default="migrationData/",
+            help="The location of the files used for load_transition_domain migration script"
+        )
+        parser.add_argument(
+            "domain_contacts_filename",
+            default="escrow_domain_contacts.daily.gov.GOV.txt",
+            help="Data file with domain contact information"
+        )
+        parser.add_argument(
+            "contacts_filename",
+            default="escrow_contacts.daily.gov.GOV.txt",
+            help="Data file with contact information",
+        )
+        parser.add_argument(
+            "domain_statuses_filename",
+            default="escrow_domain_statuses.daily.gov.GOV.txt",
+            help="Data file with domain status information"
+        )
+
+        parser.add_argument("--sep", default="|", help="Delimiter character")
+
         parser.add_argument("--debug", action=argparse.BooleanOptionalAction)
 
         parser.add_argument(
-            "--limitParse",
-            default=0,
-            help="Sets max number of entries to load, set to 0 to load all entries",
+            "--limitParse", default=0, help="Sets max number of entries to load"
+        )
+
+        parser.add_argument(
+            "--resetTable",
+            help="Deletes all data in the TransitionDomain table",
+            action=argparse.BooleanOptionalAction,
         )
 
     def print_debug_mode_statements(
-        self, debug_on: bool, debug_max_entries_to_parse: int
+        self, debug_on: bool
     ):
         """Prints additional terminal statements to indicate if --debug
         or --limitParse are in use"""
         self.print_debug(
             debug_on,
-            f"""{termColors.OKCYAN}
+            f"""{TerminalColors.OKCYAN}
             ----------DEBUG MODE ON----------
             Detailed print statements activated.
-            {termColors.ENDC}
-            """,
-        )
-        self.print_debug(
-            debug_max_entries_to_parse > 0,
-            f"""{termColors.OKCYAN}
-            ----------LIMITER ON----------
-            Parsing of entries will be limited to
-            {debug_max_entries_to_parse} lines per file.")
-            Detailed print statements activated.
-            {termColors.ENDC}
+            {TerminalColors.ENDC}
             """,
         )
 
@@ -74,21 +91,133 @@ class Command(BaseCommand):
         if print_condition:
             logger.info(print_statement)
 
+    def compare_tables(self, debug_on):
+        logger.info(
+            f"""{TerminalColors.OKCYAN}
+            ============= BEGINNING ANALYSIS ===============
+            {TerminalColors.ENDC}
+            """
+        )
+
+        #TODO: would filteredRelation be faster?
+        for transition_domain in TransitionDomain.objects.all():# DEBUG:
+            transition_domain_name = transition_domain.domain_name
+            transition_domain_email = transition_domain.username
+
+            self.print_debug(
+                debug_on,
+                f"{TerminalColors.OKCYAN}Checking: {transition_domain_name} {TerminalColors.ENDC}",  # noqa
+            )
+
+            missing_domains = []
+            duplicate_domains = []
+            missing_domain_informations = []
+            missing_domain_invites = []
+
+            # Check Domain table
+            matching_domains = Domain.objects.filter(name=transition_domain_name)
+            # Check Domain Information table
+            matching_domain_informations = DomainInformation.objects.filter(domain__name=transition_domain_name)
+            # Check Domain Invitation table
+            matching_domain_invitations = DomainInvitation.objects.filter(email=transition_domain_email.lower(), 
+                                                                          domain__name=transition_domain_name)
+
+            if len(matching_domains) == 0:
+                missing_domains.append(transition_domain_name)
+            elif len(matching_domains) > 1:
+                duplicate_domains.append(transition_domain_name)
+            if len(matching_domain_informations) == 0:
+                missing_domain_informations.append(transition_domain_name)
+            if len(matching_domain_invitations) == 0:
+                missing_domain_invites.append(transition_domain_name)
+
+        total_missing_domains = len(missing_domains)
+        total_duplicate_domains = len(duplicate_domains)
+        total_missing_domain_informations = len(missing_domain_informations)
+        total_missing_domain_invitations = len(missing_domain_invites)
+
+        missing_domains_as_string = "{}".format(", ".join(map(str, missing_domains)))
+        duplicate_domains_as_string = "{}".format(", ".join(map(str, duplicate_domains)))
+        missing_domain_informations_as_string = "{}".format(", ".join(map(str, missing_domain_informations)))
+        missing_domain_invites_as_string = "{}".format(", ".join(map(str, missing_domain_invites)))
+        
+        logger.info(
+            f"""{TerminalColors.OKGREEN}
+            ============= FINISHED ANALYSIS ===============
+            
+            {total_missing_domains} Missing Domains:
+            (These are transition domains that are missing from the Domain Table)
+            {TerminalColors.YELLOW}{missing_domains_as_string}{TerminalColors.OKGREEN}
+
+            {total_duplicate_domains} Duplicate Domains:
+            (These are transition domains which have duplicate entries in the Domain Table)
+            {TerminalColors.YELLOW}{duplicate_domains_as_string}{TerminalColors.OKGREEN}
+
+            {total_missing_domain_informations} Domains Information Entries missing:
+            (These are transition domains which have no entries in the Domain Information Table)
+            {TerminalColors.YELLOW}{missing_domain_informations_as_string}{TerminalColors.OKGREEN}
+
+            {total_missing_domain_invitations} Domain Invitations missing:
+            (These are transition domains which have no entires in the Domain Invitation Table)
+            {TerminalColors.YELLOW}{missing_domain_invites_as_string}{TerminalColors.OKGREEN}
+            {TerminalColors.ENDC}
+            """
+        )
     
+    def run_migration_scripts(self,
+                            file_location, 
+                            domain_contacts_filename,
+                            contacts_filename,
+                            domain_statuses_filename):
+
+        files_are_correct = TerminalHelper.query_yes_no(
+            f"""
+            {TerminalColors.YELLOW}
+            PLEASE CHECK: 
+            The loader scripts expect to find the following files:
+            - domain contacts: {domain_contacts_filename}
+            - contacts: {contacts_filename}
+            - domain statuses: {domain_statuses_filename}
+
+            The files should be at the following directory;
+            {file_location}
+
+            Does this look correct?{TerminalColors.ENDC}"""
+        )
+
+        if not files_are_correct:
+            # prompt the user to provide correct file inputs
+            logger.info(f"""
+            {TerminalColors.YELLOW}
+            PLEASE Re-Run the script with the correct file location and filenames: 
+            EXAMPLE:
+            
+            
+            """)
+            return
+        load_transition_domain_command.handle(
+            domain_contacts_filename,
+            contacts_filename,
+            domain_statuses_filename
+        )
+
     def handle(
         self,
+        # domain_contacts_filename,
+        # contacts_filename,
+        # domain_statuses_filename,
         **options,
     ):
         """
-        Do a diff between the transition_domain and the following tables: 
+        Does a diff between the transition_domain and the following tables: 
         domain, domain_information and the domain_invitation. 
         
-        It should:
-            - Print any domains that exist in the transition_domain table
+        Produces the following report (printed to the terminal):
+            #1 - Print any domains that exist in the transition_domain table
             but not in their corresponding domain, domain information or
             domain invitation tables.
-            - Print which table this domain is missing from
-            - Check for duplicate entries in domain or 
+            #2 - Print which table this domain is missing from
+            #3- Check for duplicate entries in domain or 
             domain_information tables and print which are 
             duplicates and in which tables
 
@@ -105,3 +234,21 @@ class Command(BaseCommand):
                 ** Triggers table reset **
         """
 
+        # Get --debug argument
+        debug_on = options.get("debug")
+        # Get --runLoaders argument
+        run_loaders_on = options.get("runLoaders")
+
+        # Analyze tables for corrupt data...
+        self.compare_tables(debug_on)
+
+        # Run migration scripts if specified by user...
+        if run_loaders_on:
+            file_location = options.get("loaderDirectory")
+            # domain_contacts_filename = options.get("domain_contacts_filename")
+            # contacts_filename = options.get("contacts_filename")
+            # domain_statuses_filename = options.get("domain_statuses_filename")
+            # self.run_migration_scripts(file_location, 
+            #                            domain_contacts_filename,
+            #                            contacts_filename,
+            #                            domain_statuses_filename)
---
+++ src/registrar/management/commands/utility/terminal_helper.py
@@ -0,0 +1,50 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+class TerminalColors:
+    """Colors for terminal outputs
+    (makes reading the logs WAY easier)"""
+
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    BackgroundLightYellow = "\033[103m"
+
+class TerminalHelper:
+
+    def query_yes_no(question: str, default="yes") -> bool:
+        """Ask a yes/no question via raw_input() and return their answer.
+
+        "question" is a string that is presented to the user.
+        "default" is the presumed answer if the user just hits <Enter>.
+                It must be "yes" (the default), "no" or None (meaning
+                an answer is required of the user).
+
+        The "answer" return value is True for "yes" or False for "no".
+        """
+        valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+        if default is None:
+            prompt = " [y/n] "
+        elif default == "yes":
+            prompt = " [Y/n] "
+        elif default == "no":
+            prompt = " [y/N] "
+        else:
+            raise ValueError("invalid default answer: '%s'" % default)
+
+        while True:
+            logger.info(question + prompt)
+            choice = input().lower()
+            if default is not None and choice == "":
+                return valid[default]
+            elif choice in valid:
+                return valid[choice]
+            else:
+                logger.info("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
