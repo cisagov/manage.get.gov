e1257608db73b9508ec24169c64306a8385732dc
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2023-10-31 11:47:22 -0600

    Fix linter issues

--- src/registrar/management/commands/cat_files_into_getgov.py
+++ src/registrar/management/commands/cat_files_into_getgov.py
@@ -70,35 +70,31 @@ class Command(BaseCommand):
         copy_from a location to copy_to a location"""
 
         # copy_from will be system defined
-        self.check_file_path(copy_from, check_directory = False)
+        self.check_file_path(copy_from, check_directory=False)
         self.check_file_path(copy_to)
 
         # This command can only be ran from inside cf ssh getgov-{sandbox}
         # It has no utility when running locally, and to exploit this
         # you would have to have ssh access anyway, which is a bigger problem.
-        exit_status = os.system(f"cat {copy_from} > {copy_to}") # nosec
+        exit_status = os.system(f"cat {copy_from} > {copy_to}")  # nosec
         return exit_status
 
-    def check_file_path(self, file_path: str, check_directory = True):
+    def check_file_path(self, file_path: str, check_directory=True):
         """Does a check on user input to ensure validity"""
         if not isinstance(file_path, str):
             raise ValueError("Invalid path provided")
-        
+
         # Remove any initial/final whitespace
         file_path = file_path.strip()
 
         # Check for any attempts to move up in the directory structure
         if ".." in file_path and check_directory:
-            raise ValueError(
-                "Moving up in the directory structure is not allowed"
-            )
+            raise ValueError("Moving up in the directory structure is not allowed")
 
         # Check for any invalid characters
         valid_chars = f"/-_.() {string.ascii_letters}{string.digits}"
         for char in file_path:
             if char not in valid_chars:
-                raise ValueError(
-                    f"Invalid character {char} in file path"
-                )
+                raise ValueError(f"Invalid character {char} in file path")
 
         return file_path
