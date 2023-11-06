6353caff0e1f92998d6c80dbb2932cc669c33a98
Erin <121973038+erinysong@users.noreply.github.com>
2023-11-06 10:11:07 -0800

    Fix linter errors

--- src/registrar/config/settings.py
+++ src/registrar/config/settings.py
@@ -299,10 +299,7 @@ CSP_FORM_ACTION = allowed_sources
 # Cross-Origin Resource Sharing (CORS) configuration
 # Sets clients that allow access control to manage.get.gov
 # TODO: remove :8080 to see if we can have all localhost access
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "https://beta.get.gov"
-]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8080", "https://beta.get.gov"]
 
 
 # Content-Length header is set by django.middleware.common.CommonMiddleware
