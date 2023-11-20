8544c2948f216e1c70feb17f6611637b7448ae2d
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2023-11-20 11:03:25 -0700

    Fix code typo, v2

--- src/registrar/management/commands/load_organization_data.py
+++ src/registrar/management/commands/load_organization_data.py
@@ -67,7 +67,7 @@ class Command(BaseCommand):
                     
                     # If any key in skipped_fields has a value, then
                     # we override what is specified in the JSON.
-                    if key not in skipped_fields or :
+                    if key not in skipped_fields:
                         options[key] = value
 
             except Exception as err:
