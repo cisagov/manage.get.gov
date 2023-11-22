ddc1298651dc3c02a7b755669dbfd7c941c1bcd8
Kristina Yin <kristina.yin@cisa.dhs.gov>
2023-11-21 19:30:36 -0800

    get rid of asterisk in no other contacts page

--- src/registrar/forms/application_wizard.py
+++ src/registrar/forms/application_wizard.py
@@ -610,8 +610,7 @@ class NoOtherContactsForm(RegistrarForm):
         required=True,
         # label has to end in a space to get the label_suffix to show
         label=(
-            "Please explain why there are no other employees from your organization"
-            " we can contact to help us assess your eligibility for a .gov domain."
+            "Please explain why there are no other employees from your organization we can contact to help us assess your eligibility for a .gov domain."
         ),
         widget=forms.Textarea(),
     )
@@ -638,4 +637,4 @@ class RequirementsForm(RegistrarForm):
             "required": ("Check the box if you read and agree to the requirements for operating .gov domains.")
         },
     )
-    
+
