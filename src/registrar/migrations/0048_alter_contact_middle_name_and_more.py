1cf841013f60bf00a73b9785ee6e37be9226be24
Kristina Yin <kristina.yin@cisa.dhs.gov>
2023-11-22 12:40:21 -0800

    fixing migration issue

--- src/registrar/migrations/0048_alter_contact_middle_name_and_more.py
+++ src/registrar/migrations/0048_alter_contact_middle_name_and_more.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("registrar", "0046_transitiondomain_email_transitiondomain_first_name_and_more"),
+        ("registrar", "0047_transitiondomain_address_line_transitiondomain_city_and_more"),
     ]
 
     operations = [
