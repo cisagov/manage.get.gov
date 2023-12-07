eaf9871dd033a73a62f1a437c96ea6a1a44c4225
Michelle Rago <60157596+michelle-rago@users.noreply.github.com>
2023-12-07 17:04:39 -0500

    Update views.py

--- src/api/views.py
+++ src/api/views.py
@@ -34,7 +34,7 @@ DOMAIN_API_MESSAGES = {
     "invalid": "Enter a domain using only letters, numbers, or hyphens (though we don't recommend using hyphens).",
     "success": "That domain is available! We’ll try to give you the domain you want, "
                "but it's not guaranteed. After you complete this form, we’ll "
-                "evaluate whether your request meets our requirements.",
+               "evaluate whether your request meets our requirements.",
     "error": GenericError.get_error_message(GenericErrorCodes.CANNOT_CONTACT_REGISTRY),
 }
 
