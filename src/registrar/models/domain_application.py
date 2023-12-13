acc853da6848472440da63e407be68dccc425dfd
David Kennedy <david.kennedy@associates.cisa.dhs.gov>
2023-12-13 16:14:44 -0500

    submission_date set on application submit; refactored index.py; display of expired in both home page and domain detail page

--- src/registrar/models/domain_application.py
+++ src/registrar/models/domain_application.py
@@ -6,6 +6,7 @@ import logging
 from django.apps import apps
 from django.db import models
 from django_fsm import FSMField, transition  # type: ignore
+from django.utils import timezone
 from registrar.models.domain import Domain
 
 from .utility.time_stamped_model import TimeStampedModel
@@ -614,6 +615,10 @@ class DomainApplication(TimeStampedModel):
         DraftDomain = apps.get_model("registrar.DraftDomain")
         if not DraftDomain.string_could_be_domain(self.requested_domain.name):
             raise ValueError("Requested domain is not a valid domain name.")
+
+        # Update submission_date to today
+        self.submission_date = timezone.now().date()
+        self.save()
 
         self._send_status_update_email(
             "submission confirmation",
