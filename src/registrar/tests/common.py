80f034766851eb6da735319edaf5f730cca75b65
Alysia Broddrick <abroddrick@truss.works>
2023-12-21 17:36:28 -0800

    Updated tests

--- src/registrar/tests/common.py
+++ src/registrar/tests/common.py
@@ -1094,6 +1094,8 @@ class MockEppLib(TestCase):
                 email=contact.email,
                 voice=contact.voice,
                 fax=contact.fax,
+                disclose=di,
+                auth_info=ai,
             )
 
     def tearDown(self):
