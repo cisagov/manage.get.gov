8fb83afcaed8d5a954d296ffd42c3a3fac94cdc9
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-01-17 14:59:10 -0700

    Update index.py

--- src/registrar/views/index.py
+++ src/registrar/views/index.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 from registrar.models import DomainApplication, Domain, UserDomainRole
-from registrar.models.draft_domain import DraftDomain
 
 
 def index(request):
