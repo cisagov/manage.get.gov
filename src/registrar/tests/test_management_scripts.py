2bbd5c4282bc6ed59ee61224254d71e4616d9100
Erin <121973038+erinysong@users.noreply.github.com>
2024-01-18 16:33:52 -0800

    Remove unused import

--- src/registrar/tests/test_management_scripts.py
+++ src/registrar/tests/test_management_scripts.py
@@ -13,7 +13,6 @@ from registrar.models import (
 )
 from registrar.models.public_contact import PublicContact
 
-from django.core.management import call_command
 from unittest.mock import patch, call
 from epplibwrapper import commands, common
 
