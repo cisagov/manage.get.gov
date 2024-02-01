cfc9a2d1695c2c6c65920225bef4ac7a52daeb72
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-02-01 11:55:35 -0700

    Test one worker

--- src/run.sh
+++ src/run.sh
@@ -6,4 +6,4 @@ set -o pipefail
 # Make sure that django's `collectstatic` has been run locally before pushing up to any environment,
 # so that the styles and static assets to show up correctly on any environment.
 
-gunicorn --worker-class=gevent --worker-connections=1000 --workers=3 registrar.config.wsgi -t 60
+gunicorn --worker-class=gevent --worker-connections=1000 --workers=1 registrar.config.wsgi -t 60
