ad6d080ad94ef7726d88d38adaa58b02cbbecc5e
Rachid Mrad <rachid.mrad@ecstech.com>
2024-02-02 15:11:22 -0500

    Add non-prod banner

---
+++ src/registrar/tests/test_environment_variables_effects.py
@@ -0,0 +1,29 @@
+from django.test import Client, TestCase, override_settings
+from django.contrib.auth import get_user_model
+
+
+class MyTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        username = "test_user"
+        first_name = "First"
+        last_name = "Last"
+        email = "info@example.com"
+        self.user = get_user_model().objects.create(
+            username=username, first_name=first_name, last_name=last_name, email=email
+        )
+        self.client.force_login(self.user)
+
+    def tearDown(self):
+        super().tearDown()
+        self.user.delete()
+
+    @override_settings(IS_PRODUCTION=True)
+    def test_production_environment(self):
+        home_page = self.client.get("/")
+        self.assertNotContains(home_page, "You are not on production.")
+
+    @override_settings(IS_PRODUCTION=False)
+    def test_non_production_environment(self):
+        home_page = self.client.get("/")
+        self.assertContains(home_page, "You are not on production.")
