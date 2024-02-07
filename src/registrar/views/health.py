f05f0c76a8a12626ccfc59ba7be815d15ae60c73
David Kennedy <david.kennedy@associates.cisa.dhs.gov>
2024-02-07 08:51:26 -0500

    debugging on health check view

--- src/registrar/views/health.py
+++ src/registrar/views/health.py
@@ -1,10 +1,13 @@
+import logging
 from django.http import HttpResponse
 
 from login_required import login_not_required
 
+logger = logging.getLogger(__name__)
 
 # the health check endpoint needs to be globally available so that the
 # PaaS orchestrator can make sure the app has come up properly
 @login_not_required
 def health(request):
+    logger.debug("in health check view")
     return HttpResponse('<html lang="en"><head><title>OK - Get.gov</title></head><body>OK</body>')
