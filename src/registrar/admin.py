3307c6994117441da21ea972961c5c032805f067
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-02-26 08:04:32 -0700

    Revert "Ticket #1776: Domain admin - Improve Domain and Domaininformation load times"

--- src/registrar/admin.py
+++ src/registrar/admin.py
@@ -758,14 +758,6 @@ class DomainInformationAdmin(ListHeaderAdmin):
     # to activate the edit/delete/view buttons
     filter_horizontal = ("other_contacts",)
 
-    autocomplete_fields = [
-        "creator",
-        "domain_application",
-        "authorizing_official",
-        "domain",
-        "submitter",
-    ]
-
     # Table ordering
     ordering = ["domain__name"]
 
@@ -1087,14 +1079,6 @@ class DomainInformationInline(admin.StackedInline):
     # For each filter_horizontal, init in admin js extendFilterHorizontalWidgets
     # to activate the edit/delete/view buttons
     filter_horizontal = ("other_contacts",)
-
-    autocomplete_fields = [
-        "creator",
-        "domain_application",
-        "authorizing_official",
-        "domain",
-        "submitter",
-    ]
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         """customize the behavior of formfields with manytomany relationships.  the customized
