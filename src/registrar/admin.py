9f7db1630c7d981da65adc5bc5cbe552afdc53b9
David Kennedy <david.kennedy@associates.cisa.dhs.gov>
2024-02-26 14:38:29 -0500

    fixed case where domain_info is None

--- src/registrar/admin.py
+++ src/registrar/admin.py
@@ -1134,7 +1134,10 @@ class DomainAdmin(ListHeaderAdmin):
     organization_name.admin_order_field = "domain_info__organization_name"  # type: ignore
 
     def custom_election_board(self, obj):
-        return "Yes" if obj.domain_info.is_election_board else "No"
+        domain_info = getattr(obj, 'domain_info', None)
+        if domain_info:
+            return "Yes" if domain_info.is_election_board else "No" 
+        return "No"
 
     custom_election_board.admin_order_field = "domain_info__is_election_board"  # type: ignore
     custom_election_board.short_description = "Election office"  # type: ignore
