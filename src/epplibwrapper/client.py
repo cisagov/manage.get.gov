6c99a19376bb4c0486ba8a0608b84b53ca50b8bb
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-03-11 12:18:14 -0600

    Update client.py

--- src/epplibwrapper/client.py
+++ src/epplibwrapper/client.py
@@ -5,6 +5,7 @@ import gevent
 from gevent.lock import BoundedSemaphore
 from collections import deque
 from gevent.pool import Pool
+from contextlib import contextmanager
 
 try:
     from epplib.client import Client
@@ -55,38 +56,125 @@ class EPPLibWrapper:
 
         self.pool_size = settings.EPP_CONNECTION_POOL_SIZE
 
-        # Lets store all threads in a common location. 
-        # Each thread contains a client with an active connection to EPP.
-        self.client_threads = []
-
         # Create the pool, then populate it with connected clients.
         self.client_pool = Pool(self.pool_size)
-        self.connection_pool_started = False
+        self.client_connection_pool_running = False
 
-        try:
-            self.populate_client_pool()
-        except RegistryError as err:
-            logger.error(f"Cannot initialize the connection pool: {err}")
-        else:
-            self.connection_pool_started = True
+        # Lets store all threads in a common location. 
+        # Each thread contains a client with an active connection to EPP.
+        self.client_threads = deque()
 
+        # Keep track of every available connection
+        self.connection_lock = BoundedSemaphore(self.pool_size)
+
+        self.populate_client_pool()
 
     def populate_client_pool(self):
+
+        # If a pool already exists, kill it.
+        if len(self.client_threads) > 0:
+            self.kill_client_pool()
+
+        self.client_connection_pool_running = False
+        try:
+            self._create_client_pool()
+        except RegistryError as err:
+            # Close old EPP connections if any exist. We want to start from a fresh slate.
+            if len(self.client_pool) > 0:
+                success = self.kill_client_pool()
+            
+            if success:
+                logger.error(f"Cannot initialize the connection pool: {err}")
+            else:
+                # TODO: Raise pool error.
+                # This could lead to a memory leak if not handled correctly.
+                raise Exception("Cannot initialize the connection pool. Existing connections could not be killed.")
+        else:
+            self.client_connection_pool_running = True
+
+    def _create_client_pool(self):
         """Given our current pool size, add a thread containing an epp client with an open epp connection"""
         for _thread_number in range(self.pool_size):
-            client_thread = self.client_pool.spawn(self._initialize_client)
-            self.client_threads.append(client_thread)
+            self._create_client_thread()
 
         # Wait for all the pools to finish spawning.
         self.client_pool.join(timeout=settings.EPP_POOL_TIMEOUT)
 
+        # Check
         for thread_number, thread in enumerate(self.client_threads, start=1):
-            # TODO: Throw some error
             if not thread.ready():
-                # TODO: Rather than raise an error, maybe we can just reduce the pool size dynamically?
                 raise RegistryError("Not all client connections were initialized within the timeout period.")
             else:
                 logger.info(f"populate_client_pool() -> Thread #{thread_number} created successfully.")
+                self.connection_lock.acquire()
+
+    def _create_client_thread(self):
+        client_thread = self.client_pool.spawn(self._initialize_client)
+        self.client_threads.append(client_thread)
+
+    def kill_client_pool(self) -> bool:
+        """Destroys an existing client pool. Closes stale connections, then removes all gevent threads."""
+
+        kill_was_successful = False
+        try:
+            # Remove stale connections
+            logger.warning("kill_client_pool() -> Killing client pool.")
+            logger.info(f"Closing stale connections: {self.client_pool}")
+            for client_thread in self.client_threads:
+                # Get the underlying client object
+                client = client_thread.value
+                # Disconnect the client by sending a disconnect command to EPP. 
+                self._disconnect(client)
+
+            # Kill all existing threads.
+            logger.info(f"Killing all threads: {self.client_pool}")
+            self.client_pool.kill()
+
+            # After killing all greenlets, clear the list to remove references.
+            self.client_threads.clear()
+
+            # Reinit the pool object itself, just for good measure
+            self.client_pool = Pool(self.pool_size)
+
+            logger.info("Client pool cleared and all connections stopped.")
+        except Exception as err:
+            logger.error(f"kill_client_pool() -> Could not kill all connections: {err}")
+        else:
+            kill_was_successful = True
+            return kill_was_successful
+
+    def kill_client_thread(self, client_thread):
+        client = client_thread.value
+        self._disconnect(client)
+        self.client_pool.killone(client_thread)
+
+    @contextmanager
+    def get_active_client_connection(self):
+        """
+        Get a connection from the pool, to make and receive traffic.
+        """
+        if not self.client_threads or len(self.client_threads) == 0:
+            self.populate_client_pool()
+
+        self.connection_lock.acquire()
+        thread = self.client_threads.popleft()
+        try:
+            client = thread.value
+            yield client
+        except TransportError:
+            # Restart the thread
+            self.kill_client_thread(thread)
+            self._create_client_thread()
+            self.connection_lock.release()
+            raise
+        except Exception:
+            # TODO - maybe restart the entire pool here
+            self.client_threads.append(thread)
+            self.connection_lock.release()
+            raise
+        else:
+            self.client_threads.append(thread)
+            self.connection_lock.release()
 
     def _initialize_client(self) -> Client:
         """Initialize a client, assuming _login defined. Sets _client to initialized
@@ -120,7 +208,6 @@ class EPPLibWrapper:
             message = "_initialize_client failed to execute due to an unknown error."
             logger.error(f"{message} Error: {err}")
             raise RegistryError(message) from err
-        
         return _client
 
     def _disconnect(self, client) -> None:
@@ -129,14 +216,14 @@ class EPPLibWrapper:
             client.send(commands.Logout())  # type: ignore
             client.close()  # type: ignore
         except Exception:
-            logger.warning("Connection to registry was not cleanly closed.")
+            logger.warning(f"Connection to registry was not cleanly for client: {client}.")
 
     def _send(self, command):
         """Helper function used by `send`."""
         cmd_type = command.__class__.__name__
 
         try:
-            _client = self._initialize_client()
+            _client = self.get_active_client_connection()
             response = _client.send(command)
         except (ValueError, ParsingError) as err:
             message = f"{cmd_type} failed to execute due to some syntax error."
