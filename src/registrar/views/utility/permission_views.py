089763c57874253a9d3132336f060e4c943c054c
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-03-18 08:07:04 -0600

    Update permission_views.py

--- src/registrar/views/utility/permission_views.py
+++ src/registrar/views/utility/permission_views.py
@@ -2,8 +2,8 @@
 
 import abc  # abstract base class
 
+from django.conf import settings
 from django.views.generic import DetailView, DeleteView, TemplateView
-from registrar.context_processors import is_production
 from registrar.models import Domain, DomainRequest, DomainInvitation
 from registrar.models.user_domain_role import UserDomainRole
 
@@ -39,8 +39,7 @@ class DomainPermissionView(DomainPermission, DetailView, abc.ABC):
         """
         if "IS_PRODUCTION" not in request.session:
             # Pass the production flag to the context
-            production_status = is_production(request)
-            request.session.update(production_status)
+            request.session["IS_PRODUCTION"] = settings.IS_PRODUCTION
         return super().dispatch(request, *args, **kwargs)
 
     # Adds context information for user permissions
