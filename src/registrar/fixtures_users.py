b249cd425f406604a14bdba32ad3142634ac60ce
Kristina Yin <140533113+kristinacyin@users.noreply.github.com>
2024-04-24 12:07:33 -0700

    remove trailing whitespace

--- src/registrar/fixtures_users.py
+++ src/registrar/fixtures_users.py
@@ -98,7 +98,7 @@ class UserFixture:
             "first_name": "Christina",
             "last_name": "Burnett",
             "email": "christina.burnett@cisa.dhs.gov",
-        }, 
+        },
         {
             "username": "012f844d-8a0f-4225-9d82-cbf87bff1d3e",
             "first_name": "Riley",
