1ca79550159911146d2885cc1f5fda5685d7d58b
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-04-26 09:53:43 -0600

    Refactor

--- src/registrar/assets/js/get-gov-admin.js
+++ src/registrar/assets/js/get-gov-admin.js
@@ -558,21 +558,35 @@ function enableRelatedWidgetButtons(changeLink, deleteLink, viewLink, elementPk,
 
 /** An IIFE for toggling the overflow styles on django-admin__model-description (the show more / show less button) */
 (function () {
+    function handleShowMoreButton(toggleButton, descriptionDiv){
+        // Check the length of the text content in the description div
+        if (descriptionDiv.textContent.length < 200) {
+            // Hide the toggle button if text content is less than 200 characters
+            // This is a little over 160 characters to give us some wiggle room if we
+            // change the font size marginally.
+            toggleButton.classList.add('display-none');
+        } else {
+            toggleButton.addEventListener('click', function() {
+                toggleShowMoreButton(toggleButton, descriptionDiv, 'dja__model-description--no-overflow')
+            });
+        }
+    }
+
+    function toggleShowMoreButton(toggleButton, descriptionDiv, showMoreClassName){
+        // Toggle the class on the description div
+        descriptionDiv.classList.toggle(showMoreClassName);
+
+        // Change the button text based on the presence of the class
+        if (descriptionDiv.classList.contains(showMoreClassName)) {
+            toggleButton.textContent = 'Show less';
+        } else {    
+            toggleButton.textContent = 'Show more';
+        }
+    }
+
     let toggleButton = document.getElementById('dja-show-more-model-description');
     let descriptionDiv = document.querySelector('.dja__model-description');
-
     if (toggleButton && descriptionDiv) {
-
-        toggleButton.addEventListener('click', function() {
-            // Toggle the class on the description div
-            descriptionDiv.classList.toggle('dja__model-description--no-overflow');
-
-            // Change the button text based on the presence of the class
-            if (descriptionDiv.classList.contains('dja__model-description--no-overflow')) {
-                toggleButton.textContent = 'Show less';
-            } else {    
-                toggleButton.textContent = 'Show more';
-            }
-        });
+        handleShowMoreButton(toggleButton, descriptionDiv)
     }
 })();
