7086884c95993d8389cbe2130a6b71c541785b6a
Erin <121973038+erinysong@users.noreply.github.com>
2024-04-29 15:48:46 -0700

    Change not checks to is None

--- src/registrar/management/commands/populate_domain_updated_federal_agency.py
+++ src/registrar/management/commands/populate_domain_updated_federal_agency.py
@@ -32,8 +32,17 @@ class Command(BaseCommand):
         "U.S. Peace Corps": "Peace Corps",
         "Chemical Safety Board": "U.S. Chemical Safety Board",
         "Nuclear Waste Technical Review Board": "U.S. Nuclear Waste Technical Review Board",
-        "State, Local, and Tribal Government": "Non-Federal Agency"
+        "State, Local, and Tribal Government": "Non-Federal Agency",
     }
+
+    def find_federal_agency_row(self, domain_object):
+        federal_agency = domain_object.federal_agency
+        # Domain Information objects without a federal agency default to Non-Federal Agency
+        if federal_agency is None:
+            federal_agency = "Non-Federal Agency"
+        if federal_agency in self.rename_deprecated_federal_agency.keys():
+            federal_agency = self.rename_deprecated_federal_agency[federal_agency]
+        return FederalAgency.objects.filter(agency=federal_agency).get()
 
     def handle(self, **options):
         """
@@ -61,43 +70,40 @@ class Command(BaseCommand):
                 federal_agency_row = self.find_federal_agency_row(domain_info)
                 domain_info.updated_federal_agency = federal_agency_row
                 domain_infos_to_update.append(domain_info)
-                logger.info(f"DomainInformation {domain_info} updated_federal_agency set to: {domain_info.updated_federal_agency}")
+                logger.info(
+                    f"DomainInformation {domain_info} updated_federal_agency set to: {domain_info.updated_federal_agency}"
+                )
             except Exception as err:
-                logger.info(f"DomainInformation for {domain_information} failed to update updated_federal_agency: {err}")
+                logger.info(
+                    f"DomainInformation for {domain_information} failed to update updated_federal_agency: {err}"
+                )
                 domain_infos_with_errors.append(domain_info)
 
-        ScriptDataHelper.bulk_update_fields(
-            DomainInformation, domain_infos_to_update, ["updated_federal_agency"]
-        )
+        ScriptDataHelper.bulk_update_fields(DomainInformation, domain_infos_to_update, ["updated_federal_agency"])
 
         for domain_request in domain_requests:
             try:
-                if not domain_request.federal_agency:
+                if domain_request.federal_agency is None:
                     domain_requests_skipped.append(domain_request)
                 else:
                     federal_agency_row = self.find_federal_agency_row(domain_request)
                     domain_request.updated_federal_agency = federal_agency_row
                     domain_requests_to_update.append(domain_request)
-                    logger.info(f"DomainRequest {domain_request} updated_federal_agency set to: {domain_request.updated_federal_agency}")
+                    logger.info(
+                        f"DomainRequest {domain_request} updated_federal_agency set to: {domain_request.updated_federal_agency}"
+                    )
             except Exception as err:
                 logger.info(f"DomainRequest for {domain_request} failed to update updated_federal_agency: {err}")
                 domain_requests_with_errors.append(domain_request)
 
-        ScriptDataHelper.bulk_update_fields(
-            DomainRequest, domain_requests_to_update, ["updated_federal_agency"]
-        )
-        
+        ScriptDataHelper.bulk_update_fields(DomainRequest, domain_requests_to_update, ["updated_federal_agency"])
+
         logger.info(f"{len(domain_infos_to_update)} DomainInformation rows updated update_federal_agency.")
-        logger.info(f"{len(domain_infos_with_errors)} DomainInformation rows errored when updating update_federal_agency.")
+        logger.info(
+            f"{len(domain_infos_with_errors)} DomainInformation rows errored when updating update_federal_agency."
+        )
         logger.info(f"{len(domain_requests_to_update)} DomainRequest rows updated update_federal_agency.")
         logger.info(f"{len(domain_requests_skipped)} DomainRequest rows with null federal_agency skipped.")
-        logger.info(f"{len(domain_requests_with_errors)} DomainRequest rows errored when updating update_federal_agency.\n{domain_requests_with_errors}")
-
-    def find_federal_agency_row(self, domain_object):
-        federal_agency = domain_object.federal_agency
-        # Domain Information objects without a federal agency default to Non-Federal Agency
-        if not federal_agency:
-            federal_agency = "Non-Federal Agency"
-        if federal_agency in self.rename_deprecated_federal_agency.keys():
-            federal_agency = self.rename_deprecated_federal_agency[federal_agency]
-        return FederalAgency.objects.filter(agency=federal_agency).get()
+        logger.info(
+            f"{len(domain_requests_with_errors)} DomainRequest rows errored when updating update_federal_agency.\n{domain_requests_with_errors}"
+        )
