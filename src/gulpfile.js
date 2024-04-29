389c75df9141c07eafa1d6a1b18c6ec0119ea169
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-04-29 09:08:56 -0600

    Update gulpfile.js

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -35,19 +35,3 @@ exports.init = uswds.init;
 exports.compile = uswds.compile;
 exports.watch = uswds.watch;
 exports.copyAssets = uswds.copyAssets
-                                                                                  
-// THIS IS A TEMPORARY WORKAROUND. Delete lines 4-17 after this lands in the node releases
-// https://github.com/nodejs/node/issues/52707#issue-2265785789
-// This solution was inspired by this change:
-// https://github.com/nodejs/node/issues/52707#issuecomment-2081352450
-/*
-async function mkDist() {
-	try { await promises.mkdir('./dist') } catch (e) { if (e.code !== 'EEXIST') throw e }
-}
-
-function copy() {
-	return src([ `${ASSETS_DIR}**`, `!${ASSETS_DIR}/**/*.{ts,tsx}`, `${ASSETS_DIR}**/*.d.ts`, ]).pipe(dest('./dist/'))
-}
-
-exports.copyAssets = series(mkDist, copy)
-*/
