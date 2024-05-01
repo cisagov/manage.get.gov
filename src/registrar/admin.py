511f432f601f64fa13ba25bc0190ec80bd45b551
Rebecca Hsieh <rebecca.hsieh@truss.works>
2024-05-01 14:56:30 -0700

    Hide display in admin page

--- src/registrar/admin.py
+++ src/registrar/admin.py
@@ -1017,7 +1017,7 @@ class DomainInformationAdmin(ListHeaderAdmin):
                 "description": "Extends type of organization",
                 "fields": [
                     "federal_type",
-                    "updated_federal_agency",
+                    # "updated_federal_agency",
                     # Above field commented out so it won't display
                     "federal_agency",  # TODO: remove later
                     "tribe_name",
@@ -1270,7 +1270,7 @@ class DomainRequestAdmin(ListHeaderAdmin):
                 "description": "Extends type of organization",
                 "fields": [
                     "federal_type",
-                    "updated_federal_agency",
+                    # "updated_federal_agency",
                     # Above field commented out so it won't display
                     "federal_agency",  # TODO: remove later
                     "tribe_name",
