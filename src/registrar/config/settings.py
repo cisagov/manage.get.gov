84e136120826c2b64c08711178aa604be6ea3370
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-05-24 12:21:20 -0600

    Revert "Ticket #2158: Use biometric auth instead of IAL2"

--- src/registrar/config/settings.py
+++ src/registrar/config/settings.py
@@ -562,11 +562,7 @@ OIDC_PROVIDERS = {
             "scope": ["email", "profile:name", "phone"],
             "user_info_request": ["email", "first_name", "last_name", "phone"],
             "acr_value": "http://idmanagement.gov/ns/assurance/ial/1",
-            # "P1" is the current IdV option; "Pb" stands for 'biometric'
-            "vtr": ["Pb", "P1"],
-            # The url that biometric authentication takes place at.
-            # A similar analog is the url for acr_value.
-            "vtm": "https://developer.login.gov/vot-trust-framework",
+            "step_up_acr_value": "http://idmanagement.gov/ns/assurance/ial/2",
         },
         "client_registration": {
             "client_id": "cisa_dotgov_registrar",
@@ -584,11 +580,7 @@ OIDC_PROVIDERS = {
             "scope": ["email", "profile:name", "phone"],
             "user_info_request": ["email", "first_name", "last_name", "phone"],
             "acr_value": "http://idmanagement.gov/ns/assurance/ial/1",
-            # "P1" is the current IdV option; "Pb" stands for 'biometric'
-            "vtr": ["Pb", "P1"],
-            # The url that biometric authentication takes place at.
-            # A similar analog is the url for acr_value.
-            "vtm": "https://developer.login.gov/vot-trust-framework",
+            "step_up_acr_value": "http://idmanagement.gov/ns/assurance/ial/2",
         },
         "client_registration": {
             "client_id": ("urn:gov:cisa:openidconnect.profiles:sp:sso:cisa:dotgov_registrar"),
