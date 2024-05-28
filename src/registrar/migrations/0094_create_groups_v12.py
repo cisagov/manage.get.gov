1f2da8f37ccd3287337d0fcedbb7c39985d1a8ee
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-05-28 08:39:32 -0600

    Revert "Merge latest and fix migrations (once again)"
    This reverts commit 069a2435fa85d2950865b9463ce01f11372ab6df.

--- src/registrar/migrations/0094_create_groups_v12.py
+++ src/registrar/migrations/0094_create_groups_v12.py
@@ -25,7 +25,7 @@ def create_groups(apps, schema_editor) -> Any:
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("registrar", "0093_alter_contact_email_alter_contact_first_name_and_more"),
+        ("registrar", "0093_alter_publiccontact_unique_together"),
     ]
 
     operations = [
