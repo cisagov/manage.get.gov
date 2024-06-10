466ba4304b2406db973d13ae0de0f3161e7286a1
Rachid Mrad <rachid.mrad@ecstech.com>
2024-06-10 14:29:00 -0400

    Make domain autocomplete in domain invitation

--- src/registrar/admin.py
+++ src/registrar/admin.py
@@ -1149,6 +1149,8 @@ class DomainInvitationAdmin(ListHeaderAdmin):
     # error.
     readonly_fields = ["status"]
 
+    autocomplete_fields = ["domain"]
+
     change_form_template = "django/admin/email_clipboard_change_form.html"
 
     # Select domain invitations to change -> Domain invitations
