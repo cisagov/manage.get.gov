a2b4665a01dc851f39b68b1ac61e2afb516f7a83
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-06-13 12:16:59 -0600

    Always show assign to me button

--- src/registrar/assets/js/get-gov-admin.js
+++ src/registrar/assets/js/get-gov-admin.js
@@ -169,12 +169,6 @@ function openInNewTab(el, removeAttribute = false){
         }
     });
 
-    // Listen to any change events, and hide the parent container if investigator has a value.
-    selector.on('change', function() {
-        // The parent container has display type flex.
-        assignSelfButton.parentElement.style.display = this.value ? "none" : "flex";
-    });
-
 })();
 /** An IIFE for pages in DjangoAdmin that use a clipboard button
 */
