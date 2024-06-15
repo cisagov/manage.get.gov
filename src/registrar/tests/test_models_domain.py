026a25cc6d4a91eced6d5f7d70e99a206aaa30b0
Rebecca Hsieh <rebecca.hsieh@truss.works>
2024-06-14 17:16:28 -0700

    Fix variable name

--- src/registrar/tests/test_models_domain.py
+++ src/registrar/tests/test_models_domain.py
@@ -1922,7 +1922,7 @@ class TestRegistrantDNSSEC(MockEppLib):
             domain, _ = Domain.objects.get_or_create(name="dnssec-dsdata.gov")
 
             # Check initial dsdata_last_change value (should be None)
-            initial_change_time = domain.dsdata_last_change
+            initial_change = domain.dsdata_last_change
 
             # Adding dnssec data
             domain.dnssecdata = self.dnssecExtensionWithDsData
@@ -1931,7 +1931,7 @@ class TestRegistrantDNSSEC(MockEppLib):
             domain = Domain.objects.get(name="dnssec-dsdata.gov")
             self.assertIsNotNone(domain.dsdata_last_change)
 
-            self.assertNotEqual(domain.dsdata_last_change, initial_change_time)
+            self.assertNotEqual(domain.dsdata_last_change, initial_change)
 
             # Get the DNS SEC extension added to the UpdateDomain command and
             # verify that it is properly sent
