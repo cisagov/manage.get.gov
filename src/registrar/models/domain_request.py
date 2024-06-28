16fbead46a532cfcdde9fd3c96731865e0063554
Rebecca Hsieh <rebecca.hsieh@truss.works>
2024-06-28 10:17:40 -0700

    Trigger run

--- src/registrar/models/domain_request.py
+++ src/registrar/models/domain_request.py
@@ -1194,7 +1194,7 @@ class DomainRequest(TimeStampedModel):
             and self._is_senior_official_complete()
             and self._is_requested_domain_complete()
             and self._is_purpose_complete()
-            # NOTE: This flag leaves submitter as empty (request wont submit) hence preset to True
+            # NOTE: This flag leaves submitter as empty (request wont submit) hence set to True
             and (self._is_submitter_complete() if not has_profile_feature_flag else True)
             and self._is_other_contacts_complete()
             and self._is_additional_details_complete()
