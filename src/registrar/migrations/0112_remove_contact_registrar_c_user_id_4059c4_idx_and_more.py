d393350a7f132eb079acf5762404656a20af9f5c
David Kennedy <david.kennedy@associates.cisa.dhs.gov>
2024-07-09 17:05:37 -0400

    fixed migrations

--- src/registrar/migrations/0112_remove_contact_registrar_c_user_id_4059c4_idx_and_more.py
+++ src/registrar/migrations/0112_remove_contact_registrar_c_user_id_4059c4_idx_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("registrar", "0109_domaininformation_sub_organization_and_more"),
+        ("registrar", "0111_create_groups_v15"),
     ]
 
     operations = [
