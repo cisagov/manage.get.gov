1433c2c31fc24fff4f1b92f41d320bdf604209a5
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-07-17 09:19:32 -0600

    Update csv_export.py

--- src/registrar/utility/csv_export.py
+++ src/registrar/utility/csv_export.py
@@ -373,7 +373,7 @@ class DomainExport(BaseExport):
             domain_type = f"{human_readable_domain_org_type} - {human_readable_domain_federal_type}"
 
         security_contact_email = model.get("security_contact_email")
-        invalid_emails = {DefaultEmail.LEGACY_DEFAULT, DefaultEmail.PUBLIC_CONTACT_DEFAULT}
+        invalid_emails = [DefaultEmail.LEGACY_DEFAULT, DefaultEmail.PUBLIC_CONTACT_DEFAULT]
         if (
             not security_contact_email
             or not isinstance(security_contact_email, str)
