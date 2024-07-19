52095f656f98adf6e01d7758814afe3f69f96e8d
David Kennedy <david.kennedy@associates.cisa.dhs.gov>
2024-07-19 17:51:20 -0400

    formatted for code readability

--- src/registrar/utility/csv_export.py
+++ src/registrar/utility/csv_export.py
@@ -344,7 +344,11 @@ class DomainExport(BaseExport):
         """
         Fetch all UserDomainRole entries and return a mapping of domain to user__email.
         """
-        user_domain_roles = UserDomainRole.objects.select_related("user").order_by("domain__name", "user__email").values_list("domain__name", "user__email")
+        user_domain_roles = (
+            UserDomainRole.objects.select_related("user")
+            .order_by("domain__name", "user__email")
+            .values_list("domain__name", "user__email")
+        )
         return list(user_domain_roles)
 
     @classmethod
