d8790448ab4cfb900d8a0cfda06a46e383fcd637
Erin Song <121973038+erinysong@users.noreply.github.com>
2024-08-29 15:45:46 -0700

    Delete submitter contacts

---
+++ src/registrar/migrations/0121_delete_cascade_submitter_contacts.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+from typing import Any
+
+# Deletes Contact objects associated with a submitter which we are deprecating
+def cascade_delete_submitter_contacts(apps, schema_editor) -> Any:
+    contacts_model = apps.get_model("registrar", "Contact")
+    submitter_contacts = contacts_model.objects.filter(submitted_domain_requests__isnull=False)
+    submitter_contacts.delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("registrar", "0120_add_domainrequest_submission_dates"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            cascade_delete_submitter_contacts,
+            reverse_code=migrations.RunPython.noop,
+            atomic=True
+        ),
+    ]
