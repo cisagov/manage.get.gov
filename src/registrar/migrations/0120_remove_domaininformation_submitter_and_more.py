f46cc141b0ec54c9de694b87d25809c7e9b2a74c
Erin Song <121973038+erinysong@users.noreply.github.com>
2024-08-29 09:57:48 -0700

    Correct migrations

--- src/registrar/migrations/0120_remove_domaininformation_submitter_and_more.py
+++ src/registrar/migrations/0120_remove_domaininformation_submitter_and_more.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("registrar", "0118_alter_portfolio_options_alter_portfolio_creator_and_more"),
+        ("registrar", "0119_remove_user_portfolio_and_more"),
     ]
 
     operations = [
