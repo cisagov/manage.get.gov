779ac92ce0a802050580285d8ed82ae4da3c2ba0
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-08-29 12:17:45 -0600

    fix migrations after merge

--- src/registrar/migrations/0122_create_groups_v16.py
+++ src/registrar/migrations/0122_create_groups_v16.py
@@ -25,7 +25,7 @@ def create_groups(apps, schema_editor) -> Any:
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("registrar", "0119_allowedemail"),
+        ("registrar", "0121_allowedemail"),
     ]
 
     operations = [
