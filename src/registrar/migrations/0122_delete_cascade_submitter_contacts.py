cf86579467705ad2aa7688457b2c0ad1a9634250
Erin Song <121973038+erinysong@users.noreply.github.com>
2024-08-29 17:23:32 -0700

    Add more conditions to cascade delete

--- src/registrar/migrations/0122_delete_cascade_submitter_contacts.py
+++ src/registrar/migrations/0122_delete_cascade_submitter_contacts.py
@@ -7,7 +7,12 @@ from typing import Any
 def cascade_delete_submitter_contacts(apps, schema_editor) -> Any:
     contacts_model = apps.get_model("registrar", "Contact")
     submitter_contacts = contacts_model.objects.filter(
-        Q(submitted_domain_requests__isnull=False) | Q(submitted_domain_requests_information__isnull=False)
+        Q(submitted_domain_requests__isnull=False)
+        | Q(submitted_domain_requests_information__isnull=False)
+        | Q(information_senior_official__isnull=True)
+        | Q(senior_official__isnull=True)
+        | Q(contact_domain_requests_information__isnull=True)
+        | Q(contact_domain_requests__isnull=True)
     )
     submitter_contacts.delete()
 
