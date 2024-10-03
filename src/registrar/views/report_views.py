965025803885eb76d8c47485f5fe4efe573949cc
Rebecca Hsieh <rebecca.hsieh@truss.works>
2024-10-03 11:53:38 -0700

    Remove print statement

--- src/registrar/views/report_views.py
+++ src/registrar/views/report_views.py
@@ -173,7 +173,6 @@ class ExportDataTypeRequests(View):
     """Returns a domain requests report for a given user on the request"""
 
     def get(self, request, *args, **kwargs):
-        print("ExportDataTypeRequests get method called")
         response = HttpResponse(content_type="text/csv")
         response["Content-Disposition"] = 'attachment; filename="domain-requests.csv"'
         csv_export.DomainRequestsDataType.exporting_dr_data_to_csv(response, request=request)
