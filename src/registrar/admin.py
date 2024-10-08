ade6733b0e99a7448dcbc9943d01567afcc9ade9
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-10-08 12:35:52 -0600

    Remove bad import

--- src/registrar/admin.py
+++ src/registrar/admin.py
@@ -5,7 +5,11 @@ from django import forms
 from django.db.models import Value, CharField, Q
 from django.db.models.functions import Concat, Coalesce
 from django.http import HttpResponseRedirect
-from registrar.utility.admin_helpers import get_action_needed_reason_default_email, get_rejection_reason_default_email
+from registrar.utility.admin_helpers import (
+    get_action_needed_reason_default_email, 
+    get_rejection_reason_default_email, 
+    get_field_links_as_list,
+)
 from django.conf import settings
 from django.shortcuts import redirect
 from django_fsm import get_available_FIELD_transitions, FSMField
@@ -21,11 +25,6 @@ from epplibwrapper.errors import ErrorCode, RegistryError
 from registrar.models.user_domain_role import UserDomainRole
 from waffle.admin import FlagAdmin
 from waffle.models import Sample, Switch
-from registrar.utility.admin_helpers import (
-    get_all_action_needed_reason_emails,
-    get_action_needed_reason_default_email,
-    get_field_links_as_list,
-)
 from registrar.models import Contact, Domain, DomainRequest, DraftDomain, User, Website, SeniorOfficial
 from registrar.utility.constants import BranchChoices
 from registrar.utility.errors import FSMDomainRequestError, FSMErrorCodes
