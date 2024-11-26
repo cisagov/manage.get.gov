bdf46b5e3f2420d25b27e2240d8401d950d1121a
Rachid Mrad <rachid.mrad@ecstech.com>
2024-11-26 12:46:31 -0500

    2594 JS

--- src/registrar/assets/src/js/getgov/requesting-entity.js
+++ src/registrar/assets/src/js/getgov/requesting-entity.js
@@ -10,9 +10,13 @@ export function handleRequestingEntityFieldset() {
     const radioFieldset = document.getElementById(`id_${formPrefix}-requesting_entity_is_suborganization__fieldset`);
     const radios = radioFieldset?.querySelectorAll(`input[name="${formPrefix}-requesting_entity_is_suborganization"]`);
     const select = document.getElementById(`id_${formPrefix}-sub_organization`);
+    const selectParent = select?.parentElement;
     const suborgContainer = document.getElementById("suborganization-container");
     const suborgDetailsContainer = document.getElementById("suborganization-container__details");
-    if (!radios || !select || !suborgContainer || !suborgDetailsContainer) return;
+    const subOrgCreateNewOption = document.getElementById("option-to-add-suborg").value
+    // Make sure all crucial page elements exist before proceeding.
+    // This more or less ensures that we are on the Requesting Entity page, and not elsewhere.
+    if (!radios || !select || !selectParent || !suborgContainer || !suborgDetailsContainer) return;
   
     // requestingSuborganization: This just broadly determines if they're requesting a suborg at all
     // requestingNewSuborganization: This variable determines if the user is trying to *create* a new suborganization or not.
@@ -28,7 +32,7 @@ export function handleRequestingEntityFieldset() {
   
     // Add fake "other" option to sub_organization select
     if (select && !Array.from(select.options).some(option => option.value === "other")) {
-        select.add(new Option("Other (enter your organization manually)", "other"));
+        select.add(new Option(subOrgCreateNewOption, "other"));
     }
   
     if (requestingNewSuborganization.value === "True") {
