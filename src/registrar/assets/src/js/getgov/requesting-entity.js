95423f294c3ed01b03dca05c60d8eea261befbd6
Rachid Mrad <rachid.mrad@ecstech.com>
2024-11-26 16:26:55 -0500

    cleanup

--- src/registrar/assets/src/js/getgov/requesting-entity.js
+++ src/registrar/assets/src/js/getgov/requesting-entity.js
@@ -7,46 +7,44 @@ import { hideElement, showElement } from './helpers.js';
 export function handleRequestingEntityFieldset() { 
     // Sadly, these ugly ids are the auto generated with this prefix
     const formPrefix = "portfolio_requesting_entity";
-    if (formPrefix) {
-        const radioFieldset = document.getElementById(`id_${formPrefix}-requesting_entity_is_suborganization__fieldset`);
-        const radios = radioFieldset?.querySelectorAll(`input[name="${formPrefix}-requesting_entity_is_suborganization"]`);
-        const select = document.getElementById(`id_${formPrefix}-sub_organization`);
-        const selectParent = select?.parentElement;
-        const suborgContainer = document.getElementById("suborganization-container");
-        const suborgDetailsContainer = document.getElementById("suborganization-container__details");
-        const subOrgCreateNewOption = document.getElementById("option-to-add-suborg")?.value;
-        // Make sure all crucial page elements exist before proceeding.
-        // This more or less ensures that we are on the Requesting Entity page, and not elsewhere.
-        if (!radios || !select || !selectParent || !suborgContainer || !suborgDetailsContainer) return;
-    
-        // requestingSuborganization: This just broadly determines if they're requesting a suborg at all
-        // requestingNewSuborganization: This variable determines if the user is trying to *create* a new suborganization or not.
-        var requestingSuborganization = Array.from(radios).find(radio => radio.checked)?.value === "True";
-        var requestingNewSuborganization = document.getElementById(`id_${formPrefix}-is_requesting_new_suborganization`);
-    
-        function toggleSuborganization(radio=null) {
-            if (radio != null) requestingSuborganization = radio?.checked && radio.value === "True";
-            requestingSuborganization ? showElement(suborgContainer) : hideElement(suborgContainer);
-            requestingNewSuborganization.value = requestingSuborganization && select.value === "other" ? "True" : "False";
-            requestingNewSuborganization.value === "True" ? showElement(suborgDetailsContainer) : hideElement(suborgDetailsContainer);
-        }
-    
-        // Add fake "other" option to sub_organization select
-        if (select && !Array.from(select.options).some(option => option.value === "other")) {
-            select.add(new Option(subOrgCreateNewOption, "other"));
-        }
-    
-        if (requestingNewSuborganization.value === "True") {
-            select.value = "other";
-        }
-    
-        // Add event listener to is_suborganization radio buttons, and run for initial display
-        toggleSuborganization();
-        radios.forEach(radio => {
-            radio.addEventListener("click", () => toggleSuborganization(radio));
-        });
-    
-        // Add event listener to the suborg dropdown to show/hide the suborg details section
-        select.addEventListener("change", () => toggleSuborganization());
+    const radioFieldset = document.getElementById(`id_${formPrefix}-requesting_entity_is_suborganization__fieldset`);
+    const radios = radioFieldset?.querySelectorAll(`input[name="${formPrefix}-requesting_entity_is_suborganization"]`);
+    const select = document.getElementById(`id_${formPrefix}-sub_organization`);
+    const selectParent = select?.parentElement;
+    const suborgContainer = document.getElementById("suborganization-container");
+    const suborgDetailsContainer = document.getElementById("suborganization-container__details");
+    const subOrgCreateNewOption = document.getElementById("option-to-add-suborg")?.value;
+    // Make sure all crucial page elements exist before proceeding.
+    // This more or less ensures that we are on the Requesting Entity page, and not elsewhere.
+    if (!radios || !select || !selectParent || !suborgContainer || !suborgDetailsContainer) return;
+
+    // requestingSuborganization: This just broadly determines if they're requesting a suborg at all
+    // requestingNewSuborganization: This variable determines if the user is trying to *create* a new suborganization or not.
+    var requestingSuborganization = Array.from(radios).find(radio => radio.checked)?.value === "True";
+    var requestingNewSuborganization = document.getElementById(`id_${formPrefix}-is_requesting_new_suborganization`);
+
+    function toggleSuborganization(radio=null) {
+        if (radio != null) requestingSuborganization = radio?.checked && radio.value === "True";
+        requestingSuborganization ? showElement(suborgContainer) : hideElement(suborgContainer);
+        requestingNewSuborganization.value = requestingSuborganization && select.value === "other" ? "True" : "False";
+        requestingNewSuborganization.value === "True" ? showElement(suborgDetailsContainer) : hideElement(suborgDetailsContainer);
     }
+
+    // Add fake "other" option to sub_organization select
+    if (select && !Array.from(select.options).some(option => option.value === "other")) {
+        select.add(new Option(subOrgCreateNewOption, "other"));
+    }
+
+    if (requestingNewSuborganization.value === "True") {
+        select.value = "other";
+    }
+
+    // Add event listener to is_suborganization radio buttons, and run for initial display
+    toggleSuborganization();
+    radios.forEach(radio => {
+        radio.addEventListener("click", () => toggleSuborganization(radio));
+    });
+
+    // Add event listener to the suborg dropdown to show/hide the suborg details section
+    select.addEventListener("change", () => toggleSuborganization());
 }
