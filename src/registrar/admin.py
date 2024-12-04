6fdb763c0249bdc46684a0d8d2e3928a442fae43
matthewswspence <Matthewswspence@gmail.com>
2024-12-04 17:10:45 -0600

    admin fix

--- src/registrar/admin.py
+++ src/registrar/admin.py
@@ -1570,7 +1570,7 @@ class DomainInformationAdmin(ListHeaderAdmin, ImportExportModelAdmin):
         "is_policy_acknowledged",
     ]
 
-    # For each filter_horizontal, init in admin js initFilterHorizontalWidget
+    # For each filter_horizontal, init in admin js extendFilterHorizontalWidgets
     # to activate the edit/delete/view buttons
     filter_horizontal = ("other_contacts",)
 
