516cac8a5ffb8d3364e3136c03275cf4ccd1cbe2
asaki222 <asaki444@gmail.com>
2024-12-05 13:22:43 -0500

    fixed the space finally VI

--- src/registrar/views/index.py
+++ src/registrar/views/index.py
@@ -8,5 +8,5 @@ def index(request):
     if request and request.user and request.user.is_authenticated:
         # This controls the creation of a new domain request in the wizard
         context["user_domain_count"] = request.user.get_user_domain_ids(request).count()
-       
+    
     return render(request, "home.html", context)
