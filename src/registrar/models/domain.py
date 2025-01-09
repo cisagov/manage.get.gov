0e1d36444f6cbfd85aed32ab6b758b11122d1d12
Rebecca Hsieh <rebecca.hsieh@truss.works>
2025-01-09 13:24:43 -0800

    If not a domain manager and in non org model mode we should display certain text

--- src/registrar/models/domain.py
+++ src/registrar/models/domain.py
@@ -1585,6 +1585,8 @@ class Domain(TimeStampedModel, DomainHelper):
             help_text = (
                 "This domain has expired, but it is still online. " "To renew this domain, contact help@get.gov."
             )
+        elif flag_is_active(self, "domain_renewal") and self.is_expiring():
+            help_text = "This domain will expire soon. Contact one of the listed domain managers to renew the domain."
         else:
             help_text = Domain.State.get_help_text(self.state)
 
