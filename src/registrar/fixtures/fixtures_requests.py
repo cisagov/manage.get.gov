43a1344c9e437eeb7a961c983a1fda56be4bb386
CocoByte <nicolle.leclair@gmail.com>
2025-01-17 13:11:21 -0700

    temporarily commit fixture for loading 100000 entries

--- src/registrar/fixtures/fixtures_requests.py
+++ src/registrar/fixtures/fixtures_requests.py
@@ -325,7 +325,7 @@ class DomainRequestFixture:
     @classmethod
     def _create_domain_requests(cls, users):  # noqa: C901
         """Creates DomainRequests given a list of users."""
-        total_domain_requests_to_make = 1000
+        total_domain_requests_to_make = 100000
 
         # Check if the database is already populated with the desired
         # number of entries.
