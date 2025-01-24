0540f0c480ac22dd635fd9141f79e76429b4a351
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2025-01-24 14:46:13 -0700

    Changes

--- src/registrar/assets/src/js/getgov/table-edit-member-domains.js
+++ src/registrar/assets/src/js/getgov/table-edit-member-domains.js
@@ -260,7 +260,7 @@ export class EditMemberDomainsTable extends BaseTable {
     // Append unassigned domains section
     if (this.removedDomains.length) {
       const unassignedHeader = document.createElement('h3');
-      unassignedHeader.classList.add('margin-bottom-1');
+      unassignedHeader.classList.add('margin-bottom-05', 'h4');
       unassignedHeader.textContent = 'Unassigned domains';
       domainAssignmentSummary.appendChild(unassignedHeader);
       domainAssignmentSummary.appendChild(unassignedDomainsList);
@@ -270,7 +270,7 @@ export class EditMemberDomainsTable extends BaseTable {
     if (this.addedDomains.length) {
       const assignedHeader = document.createElement('h3');
       // Make this h3 look like a h4
-      assignedHeader.classList.add('margin-bottom-1', 'h4');
+      assignedHeader.classList.add('margin-bottom-05', 'h4');
       assignedHeader.textContent = 'Assigned domains';
       domainAssignmentSummary.appendChild(assignedHeader);
       domainAssignmentSummary.appendChild(assignedDomainsList);
@@ -279,7 +279,7 @@ export class EditMemberDomainsTable extends BaseTable {
     // Append total assigned domains section
     const totalHeader = document.createElement('h3');
     // Make this h3 look like a h4
-    totalHeader.classList.add('margin-bottom-1', 'h4');
+    totalHeader.classList.add('margin-bottom-05', 'h4');
     totalHeader.textContent = 'Total assigned domains';
     domainAssignmentSummary.appendChild(totalHeader);
     const totalCount = document.createElement('p');
