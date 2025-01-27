e2c2813c69a8741aa1b4247b72737d995ba25a87
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2025-01-27 15:24:38 -0700

    Update email_invitations.py

--- src/registrar/utility/email_invitations.py
+++ src/registrar/utility/email_invitations.py
@@ -58,9 +58,7 @@ def send_emails_to_domain_managers(email: str, requestor_email, domain: Domain, 
         EmailSendingError
     """
     # Get each domain manager from list
-    user_domain_roles = UserDomainRole.objects.filter(
-        domain=domain,
-    )
+    user_domain_roles = UserDomainRole.objects.filter(domain=domain)
     for user_domain_role in user_domain_roles:
         # Send email to each domain manager
         user = user_domain_role.user
