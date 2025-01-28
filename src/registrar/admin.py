b124c1bb11d16aca21f596c280fc1e1153c0ed32
CuriousX <nicolle.leclair@gmail.com>
2025-01-28 10:45:41 -0700

    Update src/registrar/admin.py
    Co-authored-by: zandercymatics <141044360+zandercymatics@users.noreply.github.com>

--- src/registrar/admin.py
+++ src/registrar/admin.py
@@ -2091,7 +2091,7 @@ class DomainRequestAdmin(ListHeaderAdmin, ImportExportModelAdmin):
             )
 
             # Filter out empty values and return sorted unique entries
-            return sorted([(federal_type, DomainRequest.BranchChoices.get_branch_label(federal_type)) for federal_type in queryset if federal_type])
+            return sorted([(federal_type, BranchChoices.get_branch_label(federal_type)) for federal_type in queryset if federal_type])
 
         def queryset(self, request, queryset):
             if self.value():
