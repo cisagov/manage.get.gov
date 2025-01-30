6054b72d72bcdaca8423fc64deed1f245f248a81
Rachid Mrad <rachid.mrad@ecstech.com>
2025-01-30 17:29:11 -0500

    remove check against portfolio edit permissions from portfolio report perm mixin

--- src/registrar/views/utility/mixins.py
+++ src/registrar/views/utility/mixins.py
@@ -191,7 +191,7 @@ class PortfolioReportsPermission(PermissionsLoginMixin):
         portfolio = self.request.session.get("portfolio")
         if not self.request.user.has_view_members_portfolio_permission(
             portfolio
-        ) and not self.request.user.has_edit_members_portfolio_permission(portfolio):
+        ):
             return False
 
         return self.request.user.is_org_user(self.request)
