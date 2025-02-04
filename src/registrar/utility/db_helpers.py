7c613a01f855fe18f9c3aeb99edf0057b4f002e9
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2025-02-04 13:52:40 -0700

    revert db_helpers change

--- src/registrar/utility/db_helpers.py
+++ src/registrar/utility/db_helpers.py
@@ -9,13 +9,12 @@ def ignore_unique_violation():
     Execute within an atomic transaction so that if a unique constraint violation occurs,
     the individual transaction is rolled back without invalidating any larger transaction.
     """
-    try:
-        # NOTE - is transaction doing anything here??
-        with transaction.atomic():
+    with transaction.atomic():
+        try:
             yield
-    except IntegrityError as e:
-        if e.__cause__.pgcode == errorcodes.UNIQUE_VIOLATION:
-            # roll back to the savepoint, effectively ignoring this transaction
-            pass
-        else:
-            raise e
+        except IntegrityError as e:
+            if e.__cause__.pgcode == errorcodes.UNIQUE_VIOLATION:
+                # roll back to the savepoint, effectively ignoring this transaction
+                pass
+            else:
+                raise e
