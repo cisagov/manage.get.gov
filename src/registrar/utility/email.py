33bf1988f1700ca149403555e3125697199c9128
Rebecca Hsieh <rebecca.hsieh@truss.works>
2025-02-14 08:28:50 -0800

    Fix placement of where email subject is edited

--- src/registrar/utility/email.py
+++ src/registrar/utility/email.py
@@ -60,18 +60,7 @@ def send_templated_email(  # noqa
     # For email links ie getgov-rh.app.cloud.gov
     manage_url = env_base_url if not settings.IS_PRODUCTION else "https://manage.get.gov"
 
-    # Update the subject to have prefix here versus every email
-    subject_template = get_template(subject_template_name)
-    subject = subject_template.render(context=context)
-    subject = f"{prefix}{subject}"
-
-    # Adding to context
-    context.update(
-        {
-            "subject": subject,
-            "manage_url": manage_url,
-        }
-    )
+    context["manage_url"] = manage_url
 
     # by default assume we can send to all addresses (prod has no whitelist)
     sendable_cc_addresses = cc_addresses
@@ -94,6 +83,13 @@ def send_templated_email(  # noqa
     # Do cleanup on the email body. For emails with custom content.
     if email_body:
         email_body.strip().lstrip("\n")
+
+    # Update the subject to have prefix here versus every email
+    subject_template = get_template(subject_template_name)
+    subject = subject_template.render(context=context)
+    subject = f"{prefix}{subject}"
+
+    context["subject"] = subject
 
     try:
         ses_client = boto3.client(
