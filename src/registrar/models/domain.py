6cc477985cd4e5db1769ab828d4a798620fb0abc
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2025-03-11 12:21:46 -0600

    Update domain.py

--- src/registrar/models/domain.py
+++ src/registrar/models/domain.py
@@ -1676,31 +1676,32 @@ class Domain(TimeStampedModel, DomainHelper):
         """creates a disclose object that can be added to a contact Create using
         .disclose= <this function> on the command before sending.
         if item is security email then make sure email is visible"""
+        # Helpful Note: 'flag' is what toggles disclosure
         DF = epp.DiscloseField
         contact_disclose_map = {
             contact.ContactTypeChoices.SECURITY: {
                 "fields": [DF.EMAIL],
-                "disclose": True
+                "flag": True
             },
             contact.ContactTypeChoices.ADMINISTRATIVE: {
                 "fields": [DF.EMAIL, DF.VOICE, DF.ADDR],
                 "types": {DF.ADDR: "loc"},
-                "disclose": True
+                "flag": True
             },
             contact.ContactTypeChoices.REGISTRANT: {
                 "fields": [],
-                "disclose": False,
+                "flag": False,
             },
             contact.ContactTypeChoices.TECHNICAL: {
                 "fields": [],
-                "disclose": False,
+                "flag": False,
             }
         }
         if contact.contact_type not in contact_disclose_map:
             raise ValueError(f"_disclose_fields => Invalid or misconfigured contact type '{contact.contact_type}'")
 
-        disclose_config = contact_disclose_map.get(contact.contact_type, {"fields": [], "disclose": False})
-        logger.info("Updated domain contact %s to disclose: %s", contact.email, disclose_config.get("disclose"))
+        disclose_config = contact_disclose_map.get(contact.contact_type, {"fields": [], "flag": False})
+        logger.info("Updated domain contact %s to disclose: %s", contact.email, disclose_config.get("flag"))
         return epp.Disclose(**disclose_config)
 
     def _make_epp_contact_postal_info(self, contact: PublicContact):  # type: ignore
