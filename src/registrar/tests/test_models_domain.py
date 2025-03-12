8884baaa15794adc41b3be08711fb381c88a002b
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2025-03-12 13:32:23 -0600

    Update test_models_domain.py

--- src/registrar/tests/test_models_domain.py
+++ src/registrar/tests/test_models_domain.py
@@ -1001,7 +1001,7 @@ class TestRegistrantContacts(MockEppLib):
                     )
                 else:
                     expectedCreateCommand = self._convertPublicContactToEpp(
-                        expected_contact, disclose_email=False, disclose_fields={}
+                        expected_contact, disclose_email=False, disclose_fields=[]
                     )
                 self.mockedSendFunction.assert_any_call(expectedCreateCommand, cleaned=True)
                 # The emails should match on both items
