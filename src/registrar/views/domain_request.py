06bbfd996e35e65f9260a290315cb9de6b5f685a
Erin <121973038+erinysong@users.noreply.github.com>
2025-05-06 15:51:07 -0700

    Fix logger warning

--- src/registrar/views/domain_request.py
+++ src/registrar/views/domain_request.py
@@ -270,7 +270,7 @@ class DomainRequestWizard(TemplateView):
             try:
                 self.send_omb_submission_email()
             except Exception:
-                messages.warning(domain_request, "Could not send email confirmation to OMB.")
+                logger.warning(domain_request, "Could not send email confirmation to OMB.")
         return redirect(reverse(f"{self.URL_NAMESPACE}:finished"))
 
     def from_model(self, attribute: str, default, *args, **kwargs):
