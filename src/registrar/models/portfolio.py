e37c99b84580bf2b177c21416b8472589b191828
Aditi <aditi.green@ecstech.com>
2025-06-13 09:27:53 -0400

    added comma

--- src/registrar/models/portfolio.py
+++ src/registrar/models/portfolio.py
@@ -36,7 +36,7 @@ class Portfolio(TimeStampedModel):
     organization_name = models.CharField(
         null=True,
         blank=True,
-        unique=True
+        unique=True,
     )
 
     organization_type = models.CharField(
