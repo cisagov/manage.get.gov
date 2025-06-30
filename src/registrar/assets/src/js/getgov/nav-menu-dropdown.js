04ce55910a794c8076ca4cafc43acd4e5a93389a
Erin <121973038+erinysong@users.noreply.github.com>
2025-06-30 11:52:33 -0700

    Save WIP dropdown

---
+++ src/registrar/assets/src/js/getgov/nav-menu-dropdown.js
@@ -0,0 +1,8 @@
+function toggleNavDropdown() {
+    document.getElementById("profile-dropdown-contents").classList.toggle("show");
+}
+
+// Close dropdown when user clicks outside of dropdown
+window.onClick = function(e) {
+    
+}
