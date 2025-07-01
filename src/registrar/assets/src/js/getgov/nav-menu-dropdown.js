8d6dc1bf7268970cf2c9d5ccdb5d4a10dd551ec3
Erin <121973038+erinysong@users.noreply.github.com>
2025-07-01 13:12:28 -0700

    Add event listeners

--- src/registrar/assets/src/js/getgov/nav-menu-dropdown.js
+++ src/registrar/assets/src/js/getgov/nav-menu-dropdown.js
@@ -1,8 +1,11 @@
-function toggleNavDropdown() {
-    document.getElementById("profile-dropdown-contents").classList.toggle("show");
-}
-
-// Close dropdown when user clicks outside of dropdown
-window.onClick = function(e) {
-    
+/* 
+Initializes event listeners for custom dropdowns on secondary navbar
+since USWDS secondary navbar currently does not support dropdown menus.
+*/
+export function initializeNavMenuEventListener(menuButtonId, menuContentsId) {
+    document.getElementById(menuButtonId).addEventListener('click', function() {
+        const expanded = this.getAttribute('aria-expanded') == 'true';
+        this.setAttribute('aria-expanded', !expanded);
+        document.getElementById(menuContentsId).hidden = expanded;
+    })
 }
