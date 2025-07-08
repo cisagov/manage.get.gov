f63f6c8884317044a22db6624b49f2054f7d4aba
Erin <121973038+erinysong@users.noreply.github.com>
2025-07-08 16:12:42 -0700

    Add icon masks

--- src/registrar/assets/src/js/getgov/nav-menu-dropdown.js
+++ src/registrar/assets/src/js/getgov/nav-menu-dropdown.js
@@ -1,13 +1,16 @@
+import { toggleCaret } from './helpers.js';
 /* 
 Initializes event listeners for custom dropdowns on secondary navbar
 since USWDS secondary navbar currently does not support dropdown menus.
 */
-export function initializeNavMenuEventListener(menuButtonId, menuContentsId) {
-    document.getElementById(menuButtonId).setAttribute('aria-expanded', false);
 
-    document.getElementById(menuButtonId).addEventListener('click', function() {
-        const expanded = this.getAttribute('aria-expanded') == 'true';
-        this.setAttribute('aria-expanded', !expanded);
-        document.getElementById(menuContentsId).hidden = expanded;
-    })
-}
+// export function initializeNavMenuEventListener(menuButtonId, menuContentsId) {
+    // menuButton = document.getElementById(menuButtonId)
+    // menuButton.setAttribute('aria-expanded', false);
+
+    // document.getElementById(menuButtonId).addEventListener('click', function() {
+    //     const expanded = this.getAttribute('aria-expanded') == 'true';
+    //     this.setAttribute('aria-expanded', !expanded);
+    //     document.getElementById(menuContentsId).hidden = expanded;
+    // })
+// }
