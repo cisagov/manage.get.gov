8f510b3531d35d3a0aa72d02174d2200b914ba5c
Kristina <140533113+witha-k@users.noreply.github.com>
2025-07-17 08:20:12 -0700

    Update portfolio-member-page.js

--- src/registrar/assets/src/js/getgov/portfolio-member-page.js
+++ src/registrar/assets/src/js/getgov/portfolio-member-page.js
@@ -171,8 +171,8 @@ export function initAddNewMemberPageListeners() {
       };
       // Determine the access text based on the selected value
       let accessText = selectedAccess 
-        ? accessLevelMapping[selectedAccess.value] || "Unknown access level" 
-        : "No access level selected";
+        ? accessLevelMapping[selectedAccess.value] || "Unknown role" 
+        : "No role selected";
       // Update the modal with the appropriate member access level text
       document.getElementById('modalAccessLevel').textContent = accessText;
 
