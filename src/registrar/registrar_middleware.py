1f9e9f4513db9788d3a3a7911ef95539822ebe88
aditi-green <aditi.green@ecstech.com>
2025-07-22 18:57:02 -0400

    forgot to remove prod check

--- src/registrar/registrar_middleware.py
+++ src/registrar/registrar_middleware.py
@@ -234,17 +234,15 @@ class RequestLoggingMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):       
-        # Only log in production (stable)
-        if getattr(settings, "IS_PRODUCTION", False):
-            # Get user email (if authenticated), else None
-            user_email = request.user.email if request.user.is_authenticated else None
+        # Get user email (if authenticated), else None
+        user_email = request.user.email if request.user.is_authenticated else None
             # Get remote IP address
-            remote_ip = request.META.get("REMOTE_ADDR")
+        remote_ip = request.META.get("REMOTE_ADDR")
             # Get request path
-            request_path = request.path
+        request_path = request.path
 
             # set thread locals
-            set_user_log_context(user_email, remote_ip, request_path)
+        set_user_log_context(user_email, remote_ip, request_path)
             # Log user information
-            logger.info("Router log")
+        logger.info("Router log")
         return self.get_response(request)
