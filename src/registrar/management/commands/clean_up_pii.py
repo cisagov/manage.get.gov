4aebfb5f1db339f5a97438ec0d87a3e6cf0c4b59
aditi-green <aditi.green@ecstech.com>
2025-08-05 14:02:50 -0400

    creates to script

---
+++ src/registrar/management/commands/clean_up_pii.py
@@ -0,0 +1,4 @@
+from registrar.fixtures.fixtures_users import UserFixture
+from registrar.models import Contact, DomainRequest, DomainInvitation, PortfolioInvitation, SeniorOfficial, User
+
+Contact.objects.all()
