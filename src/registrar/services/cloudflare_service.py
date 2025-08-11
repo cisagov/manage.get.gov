cf8b387d3a69fd28ea9eac77f9682533792ff5da
Kim Allen <kim@truss.works>
2025-08-11 15:34:35 -0700

    Update secret naming and refactor create_account to use helper fn

--- src/registrar/services/cloudflare_service.py
+++ src/registrar/services/cloudflare_service.py
@@ -1,6 +1,8 @@
 import logging
-import requests
-import settings
+from django.conf import settings
+
+from registrar.utility.errors import APIError
+from registrar.utility.api_helpers import make_api_request
 
 logger = logging.getLogger(__name__)
 
@@ -10,31 +12,23 @@ class CloudflareService:
         self.base_url = "https://api.cloudflare.com/client/v4"
         self.service_email = settings.SECRET_REGISTRY_SERVICE_EMAIL
         self.tenant_key = settings.SECRET_REGISTRY_TENANT_KEY
-        self.tenant_id = settings.DOTGOV_TEST_TENANT_ACCOUNT_ID
+        self.tenant_id = settings.SECRET_DOTGOV_TENANT_ID
         self.headers = {
             "X-Auth-Email": self.service_email,
             "X-Auth-Key": self.tenant_key,
             "Content-Type": "application/json",
         }
     # POST account
-    def create_account(self, account_name):     
-        try:
-            response = requests.post(
-                    f"{self.base_url}/accounts",
-                    headers=self.headers,
-                    json={"name": account_name, "type": "enterprise", "unit": {"id": self.tenant}},
-                    timeout=5,
-                )
-            response_json = response.json()
-            response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            logger.debug("Failed to create host account", e)
-        
-        
-        account_id = response.json()["result"]["id"]        
-        logger.info(f"Created host account: {response_json}")
+    def create_account(self, account_name)-> str:     
+        url = f"{self.base_url}/accounts"
+        data = {"name": account_name, "type": "enterprise", "unit": {"id": self.tenant_id}}
+        response = make_api_request(url=url, method="POST", headers=self.headers, data=data )
+        if not response['success']:
+            raise APIError(f"Failed to create account for {account_name}: {response['message']}")
+               
+        logger.info(f"Created host account: {response['data']}")
 
-        return account_id
+        return response['data']
 
     # POST zone
 
