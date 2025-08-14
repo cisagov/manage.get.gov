ee48c90423d3737f743d843c7421ae7d6de01ef5
abe-alam-ecs <abraham.alam@ecstech.com>
2025-08-14 12:39:01 -0400

    test change to py file

--- src/registrar/tests/test_admin.py
+++ src/registrar/tests/test_admin.py
@@ -346,6 +346,7 @@ class TestDomainInvitationAdmin(WebTest):
             self.assertContains(response, "retrieved", count=4)
             self.assertContains(response, "Retrieved", count=2)
 
+            # adding comment to py file. 
             # Check for the HTML context specificially
             invited_html = '<a id="status-filter-invited" href="?status__exact=invited">Invited</a>'
             retrieved_html = '<a id="status-filter-retrieved" href="?status__exact=retrieved">Retrieved</a>'
