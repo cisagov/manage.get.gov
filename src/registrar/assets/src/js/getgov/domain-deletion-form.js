b7b405bef795a0a8da1bb6c40fafb297862652d1
aditi-green <aditi.green@ecstech.com>
2025-09-12 10:01:01 -0400

    changes so far again

--- src/registrar/assets/src/js/getgov/domain-deletion-form.js
+++ src/registrar/assets/src/js/getgov/domain-deletion-form.js
@@ -3,15 +3,16 @@ import { submitForm } from './form-helpers.js';
 export function domainDeletionEventListener() {
     const request_submit_button = document.getElementById("domain-deletion-submit-button")
     const checkbox = document.getElementById("delete-domain-checkbox")
-    const deletionModal = document.getElementById("toggle-delete-domain")
-    const submit_form = document.getElementById("submit-domain-deletion-form")
+
     if(request_submit_button) {
          request_submit_button.addEventListener("click", function (e) {
-           e.preventDefault();
-           submitForm("submit-domain-deletion-form");
-           if(checkbox.checked){
-            console.log(" ARE WE HERE?!")
-            deletionModal.toggleModal(undefined, true)
+           if(!checkbox.checked){
+            submitForm("submit-domain-deletion-form");
+           }
+           else{
+              request_submit_button.setAttribute('href','#toggle-delete-domain')       
+              request_submit_button.setAttribute('aria-controls', "toggle-delete-domain")
+              request_submit_button.click()
            }
         });
     } 
