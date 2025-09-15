8d01b011061ba30342061d580ef0c2289487ecdc
Erin <121973038+erinysong@users.noreply.github.com>
2025-09-15 16:53:21 -0700

    Skip portfolio organizations dropdown template tag on callback test

--- src/registrar/templatetags/nav_header.py
+++ src/registrar/templatetags/nav_header.py
@@ -1,7 +1,7 @@
 """Loads user portfolio data to display on Organizations nav dropdown."""
 
 from django import template
-from registrar.models import UserPortfolioPermission
+from registrar.models import UserPortfolioPermission, User
 
 register = template.Library()
 
@@ -9,5 +9,7 @@ register = template.Library()
 @register.inclusion_tag("includes/portfolio_organizations_dropdown.html", takes_context=True)
 def portfolio_organizations_dropdown(context):
     user = context["user"]
-    return {"user_portfolio_permissions": UserPortfolioPermission.objects.filter(user=user).order_by("portfolio")}
+    # Ignore incomplete MagicMock user created in test_login_callback_does_not_requires_step_up_auth
+    if user.__class__.__name__ == User:
+        return {"user_portfolio_permissions": UserPortfolioPermission.objects.filter(user=user).order_by("portfolio")}
 
