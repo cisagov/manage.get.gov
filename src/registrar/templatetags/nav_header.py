45deb205f5e6baab2bca5c496da1b5f7a43529ff
Erin <121973038+erinysong@users.noreply.github.com>
2025-09-15 17:00:55 -0700

    Run linter

--- src/registrar/templatetags/nav_header.py
+++ src/registrar/templatetags/nav_header.py
@@ -12,4 +12,3 @@ def portfolio_organizations_dropdown(context):
     # Ignore incomplete MagicMock user created in test_login_callback_does_not_requires_step_up_auth
     if user.__class__.__name__ == User:
         return {"user_portfolio_permissions": UserPortfolioPermission.objects.filter(user=user).order_by("portfolio")}
-
