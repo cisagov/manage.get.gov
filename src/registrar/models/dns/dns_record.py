1216714e235159f6ea5d0839c36c86614902ca3e
Kim Allen <kim@truss.works>
2025-10-07 09:09:45 -0700

    Satisfy flake8

--- src/registrar/models/dns/dns_record.py
+++ src/registrar/models/dns/dns_record.py
@@ -4,4 +4,5 @@ from ..utility.time_stamped_model import TimeStampedModel
 
 class DnsRecord(TimeStampedModel):
     dns_zone = models.ForeignKey("DnsZone", on_delete=models.CASCADE)
-    vendor_dns_record = models.ManyToManyField("registrar.VendorDnsRecord", through="DnsRecord_VendorDnsRecord")  # type: ignore
+    vendor_dns_record = models.ManyToManyField("registrar.VendorDnsRecord",
+                                               through="DnsRecord_VendorDnsRecord")  # type: ignore
