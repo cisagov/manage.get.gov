7281baf51830545411a4e6550e87a60e0e3bfd72
Kim Allen <kim@truss.works>
2025-10-07 09:16:26 -0700

    Satisfy black

--- src/registrar/models/dns/dns_account.py
+++ src/registrar/models/dns/dns_account.py
@@ -4,5 +4,6 @@ from ..utility.time_stamped_model import TimeStampedModel
 
 class DnsAccount(TimeStampedModel):
     name = models.CharField(unique=True, max_length=255)
-    vendor_dns_account = models.ManyToManyField("registrar.VendorDnsAccount",
-                                                through="DnsAccount_VendorDnsAccount")  # type: ignore
+    vendor_dns_account = models.ManyToManyField(
+        "registrar.VendorDnsAccount", through="DnsAccount_VendorDnsAccount"
+    )  # type: ignore
