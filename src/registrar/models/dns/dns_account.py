845b92dd72b07ff5d8bea4182a3dd393dbc3451b
Kim Allen <kim@truss.works>
2025-10-09 10:51:00 -0700

    DNS Hosting: new models and migration (#4247)
    * Set up account tables and vendor table
    
    * Set up zone tables
    
    * Fix file name and add models to init file
    
    * Add dns record tables
    
    * Add one to many relationships; use TimeStampedModel for join tables
    
    * Revert django-model-2puml package to one before skip-choices-fields bug
    
    * Make migration and update models diagram
    
    * Fix black lint errors
    
    * Fix join table imports and TimeStamp
    
    * Fix more linting issues and remove extra file
    
    * Redo migration and model diagram
    
    * Rerun migration to align timestamps with previous migrations
    
    * Update a comment to trigger CI checks
    
    * Satisfy mypi linter
    
    * Satisfy flake8
    
    * Satisfy black
    
    * Add related_names; use one-to-one for domain; adjust x id names
    
    * Redo migration
    
    * Update diagram
    
    * Add comment to trigger CI checks
    
    * Remove unused imports
    
    * Fix lint issues

---
+++ src/registrar/models/dns/dns_account.py
@@ -0,0 +1,9 @@
+from django.db import models
+from ..utility.time_stamped_model import TimeStampedModel
+
+
+class DnsAccount(TimeStampedModel):
+    name = models.CharField(unique=True, max_length=255)
+    vendor_dns_account = models.ManyToManyField(
+        "registrar.VendorDnsAccount", through="DnsAccount_VendorDnsAccount", related_name="accounts"
+    )  # type: ignore
