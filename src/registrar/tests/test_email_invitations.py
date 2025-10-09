6088ce3cd5de40ea0b284c8409698729864d3693
aditi-green <aditi.green@ecstech.com>
2025-10-09 13:03:11 -0400

    tests so far

--- src/registrar/tests/test_email_invitations.py
+++ src/registrar/tests/test_email_invitations.py
@@ -1395,37 +1395,71 @@ class TestDomainRenewalNotificationEmail(unittest.TestCase):
     Unit test for send_domain_renewal_notification_emails function
     """
     def setUp(self):
-        self.domain = MagicMock(spec=Domain, expiration_date=date.today())
-        self.portfolio = MagicMock(spec=Portfolio)
-        self.domain_manager = MagicMock(spec=User, email="domain_manager@example.gov")
-        self.portfolio_admin_user = MagicMock(spec=User, email="portfolio_admin@example.gov")
+        #users 
+        self.user_1 = MagicMock()
+        self.user_1.email = "user1@example.gov"
+        self.user_2 = MagicMock(spec=User)
+        self.user_2.email = "user2@example.gov"
 
-        self.portfolio_admin = MagicMock(spec=UserDomainRole)
-        self.portfolio_admin.user = self.portfolio_admin_user
-        self.portfolio_admin.roles = [UserPortfolioRoleChoices.ORGANIZATION_ADMIN]
-        
-        self.domain_manager_role = MagicMock(spec=UserDomainRole)
-        self.domain_manager_role.user = self.domain_manager
+        #domain 
+        self.domain = MagicMock()
+        self.domain.name = "domainrenewal.gov"
+        self.domain.expiration_date = date.today()
+        domain_role = MagicMock()
+        domain_role.domain = self.domain
+        domain_role.user = self.user_1
+        domain_role.role = UserDomainRole.Roles.MANAGER
+
+        #portfolio
+        self.portfolio = MagicMock()
+        self.user_permissions = MagicMock()
+        self.user_permissions.user = self.user_2
+        self.user_permissions.portfolio = self.portfolio
+        self.user_permissions.roles = [UserPortfolioRoleChoices.ORGANIZATION_ADMIN]
+       
+        #domain information
+        self.domain_info = MagicMock()
+        self.domain_info.domain = self.domain
+        self.domain_info.portfolio = self.portfolio
 
     @less_console_noise_decorator
     @patch("registrar.utility.email_invitations.send_templated_email")
-    def test_send_email_successful(self, mock_filter, mock_send_templated_email):
-        result = send_domain_renewal_notification_emails(domain=self.domain)
-        mock_send_templated_email.return_value = None
+    @patch("registrar.utility.email_invitations.UserDomainRole.objects.filter")
+    @patch("registrar.utility.email_invitations.DomainInformation.objects.filter")
+    @patch("registrar.utility.email_invitations.Portfolio.objects")
+    def test_send_email_success(self, mock_domain_role_filter, mock_domain_information_filter, mock_portfolio, mock_send_templated_email):
+        """Test successful sending of domain renewal emails."""
+        mock_queryset_domain_role = MagicMock()
+        mock_queryset_domain_role.values_list.return_value.distinct.return_value = [self.user_1.email]
+        mock_domain_role_filter.return_value = mock_queryset_domain_role
+    
 
-        mock_filter.assert_called_once_with(domain=self.domain)
+        mock_queryset_domain_info = MagicMock()
+        mock_queryset_domain_info.first.return_value = [self.domain_info]
+        mock_domain_information_filter.return_value = mock_queryset_domain_info 
+        
+        mock_queryset_portfolio_admin = MagicMock()
+        mock_queryset_portfolio_admin.portfolio_admin_users.return_value.values_list.distinct.return_value = [self.user_2.email]
+        mock_portfolio.return_value = mock_queryset_portfolio_admin
 
-        mock_send_templated_email.assert_any_call(
-                            "emails/domain_renewal_success.txt",
-                            "emails/domain_renewal_success_subject.txt",
-                            to_addresses=[self.domain_manager.email, self.portfolio_admin_user.email],
-                            bcc_address="",
-                            context={
-                                "domain":self.domain,
-                                "expiration_date": self.domain.expiration_date
-                            }
 
+        mock_send_templated_email.return_value = None  # No exception means success
+
+        result = send_domain_renewal_notification_emails(
+            domain=self.domain,
         )
 
-        self.assertTrue(result)
+        mock_domain_role_filter.assert_called_once_with(domain=self.domain)
+        mock_domain_information_filter.assert_called_once_with(domain=self.domain)
+        mock_portfolio.assert_called_once_with(domain=self.domain)
+        mock_send_templated_email.assert_any_call(
+            "emails/domain_renewal_success.txt",
+            "emails/domain_renewal_success_subject.txt",
+            to_addresses=[self.user_1.email, self.user_2.email],
+            context={
+                "domain": self.domain,
+                "expiration_date": self.domain.expiration_date
+            }
 
+        )
+        self.assertTrue(result)
