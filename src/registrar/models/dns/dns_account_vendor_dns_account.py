a74331d3f379ed90a2eeda6b22c157a3447a9b1b
Abe Alam <143724440+abe-alam-ecs@users.noreply.github.com>
2025-10-15 14:35:26 -0400

    initial model and tests

--- src/registrar/models/dns/dns_account_vendor_dns_account.py
+++ src/registrar/models/dns/dns_account_vendor_dns_account.py
@@ -1,9 +1,26 @@
-from django.db.models import ForeignKey, CASCADE
+from django.db import models
+from django.db.models import Q, UniqueConstraint
 from ..utility.time_stamped_model import TimeStampedModel
 
 
 class DnsAccount_VendorDnsAccount(TimeStampedModel):
-    dns_account = ForeignKey("registrar.DnsAccount", on_delete=CASCADE, related_name="account_link")
-    vendor_dns_account = ForeignKey(
-        "registrar.VendorDnsAccount", on_delete=CASCADE, related_name="account_link"
+    dns_account = models.ForeignKey(
+          "registrar.DnsAccount", 
+          on_delete=models.CASCADE, 
+          related_name="account_link")
+    vendor_dns_account = models.ForeignKey(
+        "registrar.VendorDnsAccount", 
+        on_delete=models.CASCADE, 
+        related_name="account_link"
     )  # type: ignore
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+            constraints = [
+                  UniqueConstraint(
+                        fields=["dns_account"], 
+                        condition=Q(is_active=True),
+                        name="unique_active_vendor_per_dns_account",
+                  ),
+            ]
+
