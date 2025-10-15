d9ae66c23c9fa40a11e68943c15138836223c694
Erin <121973038+erinysong@users.noreply.github.com>
2025-10-15 15:34:08 -0700

    Update migration dependency

--- src/registrar/migrations/0159_portfolio_agency_seal.py
+++ src/registrar/migrations/0159_portfolio_agency_seal.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("registrar", "0158_dnsaccount_dnsrecord_dnsvendor_dnszone_and_more.py"),
+        ("registrar", "0158_dnsaccount_dnsrecord_dnsvendor_dnszone_and_more"),
     ]
 
     operations = [
