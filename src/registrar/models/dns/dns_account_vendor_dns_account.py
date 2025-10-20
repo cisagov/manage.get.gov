af4023e4ac9c960bd386816eb7d7e7e0cc199d51
Abe Alam <143724440+abe-alam-ecs@users.noreply.github.com>
2025-10-20 17:06:17 -0400

    Create new migration to untangle numbering Update testing (naming and functionality) Linter fixes

--- src/registrar/models/dns/dns_account_vendor_dns_account.py
+++ src/registrar/models/dns/dns_account_vendor_dns_account.py
@@ -4,23 +4,17 @@ from ..utility.time_stamped_model import TimeStampedModel
 
 
 class DnsAccount_VendorDnsAccount(TimeStampedModel):
-    dns_account = models.ForeignKey(
-          "registrar.DnsAccount", 
-          on_delete=models.CASCADE, 
-          related_name="account_link")
+    dns_account = models.ForeignKey("registrar.DnsAccount", on_delete=models.CASCADE, related_name="account_link")
     vendor_dns_account = models.ForeignKey(
-        "registrar.VendorDnsAccount", 
-        on_delete=models.CASCADE, 
-        related_name="account_link"
+        "registrar.VendorDnsAccount", on_delete=models.CASCADE, related_name="account_link"
     )  # type: ignore
     is_active = models.BooleanField(default=True)
 
     class Meta:
-            constraints = [
-                  UniqueConstraint(
-                        fields=["dns_account"], 
-                        condition=Q(is_active=True),
-                        name="unique_active_vendor_per_dns_account",
-                  ),
-            ]
-
+        constraints = [
+            UniqueConstraint(
+                fields=["dns_account"],
+                condition=Q(is_active=True),
+                name="unique_active_vendor_per_dns_account",
+            ),
+        ]
