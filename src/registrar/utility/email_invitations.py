b019d96324242be6799d7ff82b87243597bce3cd
Charlie Wells <charles.wells@ecstech.com>
2025-10-20 16:39:42 -0400

    #4167: Black formatting fixes by black itself

--- src/registrar/utility/email_invitations.py
+++ src/registrar/utility/email_invitations.py
@@ -634,9 +634,13 @@ def _send_portfolio_admin_removal_emails_to_portfolio_admins(email: str, request
     """
     all_emails_sent = True
     # Get each portfolio admin from list
-    user_portfolio_permissions = UserPortfolioPermission.objects.filter(
-        portfolio=portfolio, roles__contains=[UserPortfolioRoleChoices.ORGANIZATION_ADMIN], user__isnull=False
-    ).exclude(user__email=email).select_related("user")
+    user_portfolio_permissions = (
+        UserPortfolioPermission.objects.filter(
+            portfolio=portfolio, roles__contains=[UserPortfolioRoleChoices.ORGANIZATION_ADMIN], user__isnull=False
+        )
+        .exclude(user__email=email)
+        .select_related("user")
+    )
     for user_portfolio_permission in user_portfolio_permissions:
         # Send email to each portfolio_admin
         user = user_portfolio_permission.user
