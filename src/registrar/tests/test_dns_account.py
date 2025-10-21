b7ecb643f869caf19a36dd044b1ec972d361b92f
Abe Alam <143724440+abe-alam-ecs@users.noreply.github.com>
2025-10-21 16:11:04 -0400

    updates to test names

--- src/registrar/tests/test_dns_account.py
+++ src/registrar/tests/test_dns_account.py
@@ -6,21 +6,18 @@ DnsAccount = apps.get_model("registrar", "DnsAccount")
 
 
 class DnsAccountTest(TestCase):
-    """
-    Testing constraints on DnsAccount model
-    """
 
     def setUp(self):
         self.dns_account = DnsAccount.objects.create(name="acct-base")
         self.dns_account.save()
 
-    def test_dns_account_creation(self):
+    def test_dns_account_creation_success(self):
         try:
             DnsAccount.objects.create(name="second-acct")
         except IntegrityError as e:
             self.fail(f"Unexpected IntegrityError for unique name: {e}")
 
-    def test_dns_account_name_is_unique(self):
+    def test_dns_account_name_is_not_unique_throws_error(self):
         with self.assertRaises(IntegrityError):
             with transaction.atomic():
                 DnsAccount.objects.create(name=self.dns_account.name)
