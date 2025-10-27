ce13e9ebdf4012f20c93b917a3cd1ef15089bd05
Erin <121973038+erinysong@users.noreply.github.com>
2025-10-27 10:06:54 -0700

    Fix linting

--- src/registrar/models/dns/dns_zone_vendor_dns_zone.py
+++ src/registrar/models/dns/dns_zone_vendor_dns_zone.py
@@ -5,7 +5,11 @@ from ..utility.time_stamped_model import TimeStampedModel
 class DnsZone_VendorDnsZone(TimeStampedModel):
     class Meta:
         constraints = [
-            UniqueConstraint(fields=["dns_zone"], condition=Q(is_active=True), name="unique_active_vendor_zone_per_dns_zone")
+            UniqueConstraint(
+                fields=["dns_zone"],
+                condition=Q(is_active=True),
+                name="unique_active_vendor_zone_per_dns_zone"
+            )
         ]
 
     dns_zone = ForeignKey("registrar.DnsZone", on_delete=CASCADE, related_name="zone_link")
