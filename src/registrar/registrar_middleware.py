6e2a8a0264f41e9cd4bc6e5fa22d5048e8e904ab
Daisy Gutierrez <daisyguti56@gmail.com>
2025-10-29 12:09:23 -0700

    fix for port not always in session

--- src/registrar/registrar_middleware.py
+++ src/registrar/registrar_middleware.py
@@ -171,7 +171,8 @@ class CheckPortfolioMiddleware:
 
         # Allow legacy users to see old home
         if current_path == self.home and request.GET.get(self.legacy_home) == "1" and request.user.has_legacy_domain():
-            del request.session["portfolio"]
+            for k in ("portfolio",):
+                request.session.pop(k, None)            
             return None
 
         # Assign user portfolio if:
