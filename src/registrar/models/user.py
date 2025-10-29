ffbdf92e55624a28063c657dda8b5a42e742cfee
asaki222 <130692870+asaki222@users.noreply.github.com>
2025-10-29 14:25:23 -0400

    4035 - Update the query to look for non case sensitive match - [AD] (#4293)
    * update the query to look for non case sensitive match
    
    * added another update to the filter
    
    * update to requested changes
    
    * updated to requested changes made a logic error
    
    * ran formatter
    
    * Revert "updated to requested changes made a logic error"
    
    This reverts commit 9b57bb01ad2f07c78b76e1dee332898adf69bcb6.
    
    * made requested changes fixed the logic
    
    * missed adding a variable
    
    * tried a different approach after confirming that the email addresses are lowercase
    
    * possibly adding back i exact to cover all use cases
    
    * another approach to solving this bug that is future defensive
    
    * ran formatter
    
    * removed i exact
    
    * updated test to be case sensitive
    
    * adjusted logic taken to account another scenario for normalization
    
    * ran app black
    
    * put back iexact queries
    
    * added a migration file
    
    * Revert "added a migration file"
    
    This reverts commit ba544a2001a618b421d60e49fec9a4d3ca12fde2.
    
    * fixed portfolioinvitation query to be case insensitive for member of a different org
    
    * added .lower()
    
    * added fix for portfolio invitation admin table
    
    * Revert "added fix for portfolio invitation admin table"
    
    This reverts commit 1f724e5beda77f99b6fcda36041c898942f6527a.
    
    * update code with portfolio invitation fixes
    
    * update test
    
    * ran docker compose black
    
    * moved error messages to a message
    
    * added retrieved on the filter
    
    * added the retrieved status exclusion
    
    * added casing test
    
    * added casing test
    
    * fixed typo
    
    * integrated a test for diff capitalization for portfolio invitations

--- src/registrar/models/user.py
+++ src/registrar/models/user.py
@@ -475,3 +475,8 @@ class User(AbstractUser):
 
         # If there are other admins or the user is not the only one
         return False
+
+    def save(self, *args, **kwargs):
+        if self.email:
+            self.email = self.email.lower()
+        super().save(*args, **kwargs)
