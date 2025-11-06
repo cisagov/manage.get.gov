33d0c28d6f107c85df439794b2c6bce342b330ab
Erin <121973038+erinysong@users.noreply.github.com>
2025-11-06 10:51:25 -0800

    Remove unused imports

--- src/registrar/models/dns/dns_record.py
+++ src/registrar/models/dns/dns_record.py
@@ -1,6 +1,5 @@
 from django.db import models
 from ..utility.time_stamped_model import TimeStampedModel
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.postgres.fields import ArrayField
 from django.core.exceptions import ValidationError
 
