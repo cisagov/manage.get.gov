e7bd1813751bcd4d0d984770ee4b7383478b4d54
Erin <121973038+erinysong@users.noreply.github.com>
2025-11-20 16:33:39 -0800

    Remove unused import

--- src/registrar/config/settings.py
+++ src/registrar/config/settings.py
@@ -396,7 +396,7 @@ CONTENT_SECURITY_POLICY = {
             "https://cdn.jsdelivr.net/npm/chart.js",
             "https://www.ssa.gov",
             "https://ajax.googleapis.com",
-            "https://cdn.jsdelivr.net",
+            "https://cdn.jsdelivr.net/npm/@alpinejs",
         ],
         "style-src": [SELF, NONCE, "https://www.ssa.gov/accessibility/andi/andi.css"],
     }
