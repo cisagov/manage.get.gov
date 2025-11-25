488b0a25c937f9cef3dd3b23baa2a892d091eecd
asaki222 <130692870+asaki222@users.noreply.github.com>
2025-11-25 12:55:06 -0500

    Quick fix with Domain Request IN REVIEW - OMB Status - [POTATO] (#4383)

--- src/registrar/admin.py
+++ src/registrar/admin.py
@@ -492,10 +492,17 @@ class DomainRequestAdminForm(forms.ModelForm):
         in the FSM. Allows us to still display fields filtered out by a condition."""
         curr_state = field.get_state(instance)
         transitions = field.transitions[instance.__class__]
+        is_feb_request = instance.is_feb() or instance.federal_type == BranchChoices.EXECUTIVE
 
         for name, transition in transitions.items():
             meta = transition._django_fsm
             if meta.has_transition(curr_state):
+                transition_data = meta.get_transition(curr_state)
+
+                # exclude in review - omb as a status for non feb requests
+                if not is_feb_request and transition_data.target == DomainRequest.DomainRequestStatus.IN_REVIEW_OMB:
+                    continue
+
                 yield meta.get_transition(curr_state)
 
     def clean(self):
@@ -522,6 +529,7 @@ class DomainRequestAdminForm(forms.ModelForm):
             DomainRequest.DomainRequestStatus.ACTION_NEEDED,
             DomainRequest.DomainRequestStatus.REJECTED,
             DomainRequest.DomainRequestStatus.INELIGIBLE,
+            DomainRequest.DomainRequestStatus.IN_REVIEW_OMB,
         ]
 
         # If a status change occured, check for validity
@@ -3334,6 +3342,7 @@ class DomainRequestAdmin(ListHeaderAdmin, ImportExportRegistrarModelAdmin):
             models.DomainRequest.DomainRequestStatus.WITHDRAWN: domain_request.withdraw,
             models.DomainRequest.DomainRequestStatus.REJECTED: domain_request.reject,
             models.DomainRequest.DomainRequestStatus.INELIGIBLE: (domain_request.reject_with_prejudice),
+            models.DomainRequest.DomainRequestStatus.IN_REVIEW_OMB: domain_request.in_review_omb,
         }
 
         # Grab the method
