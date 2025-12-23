db101543c38d0ec499c5644cfac87e62a6c77df6
aditi-green <aditi.green@ecstech.com>
2025-12-23 15:42:31 -0500

    added the fi

--- ops/scripts/write_git_info_to_env.sh
+++ ops/scripts/write_git_info_to_env.sh
@@ -8,9 +8,10 @@ cf api api.fr.cloud.gov
 cf auth "$CF_USERNAME" "$CF_PASSWORD"
 cf target -o cisa-dotgov -s $ENVIRONMENT
 if [[ "$ENVIRONMENT" == "stable" || "$ENVIRONMENT" == "staging"]]; then
-    APP_NAME=$ENVIRONMENT
+    APP_NAME="$ENVIRONMENT"
 else
     APP_NAME="getgov-${ENVIRONMENT}"
+fi
 
 
 cf set-env $APP_NAME GIT_BRANCH "$BRANCH"
