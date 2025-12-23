dc9d4f8d0dba166b9c996aec71f7831ecf5a6726
aditi-green <aditi.green@ecstech.com>
2025-12-23 14:54:43 -0500

    updated workflow again

--- ops/scripts/write_git_info_to_env.sh
+++ ops/scripts/write_git_info_to_env.sh
@@ -9,6 +9,10 @@ echo " Branch: $BRANCH"
 echo " Commit: $COMMIT"
 echo " TAG: ${TAG:-none}"
 
+echo "Authenticating"
+cf api api.fr.cloud.gov
+cf auth "$CF_USERNAME" "$CF_PASSWORD"
+cf target -o cisa-dotgov -s $ENVIRONMENT
 cf set-env $ENVIRONMENT GIT_BRANCH "$BRANCH"
 cf set-env $ENVIRONMENT GIT_COMMIT "$COMMIT"
 cf set-env $ENVIRONMENT GIT_TAG "$TAG"
