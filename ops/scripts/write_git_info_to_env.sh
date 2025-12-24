0f7641fb4e0fa85eef611c10f21faecb9b6f37fe
aditi-green <aditi.green@ecstech.com>
2025-12-24 14:38:44 -0500

    removed script from file

--- ops/scripts/write_git_info_to_env.sh
+++ ops/scripts/write_git_info_to_env.sh
@@ -5,10 +5,6 @@ echo " Branch: $BRANCH"
 echo " Commit: $COMMIT"
 echo " TAG: ${TAG:-none}"
 
-echo "Authenticating"
-cf api api.fr.cloud.gov
-cf auth "$CF_USERNAME" "$CF_PASSWORD"
-cf target -o cisa-dotgov -s $ENVIRONMENT
 if [[ "$ENVIRONMENT" == "stable" || "$ENVIRONMENT" == "staging" ]]; then
     APP_NAME="$ENVIRONMENT"
 else
