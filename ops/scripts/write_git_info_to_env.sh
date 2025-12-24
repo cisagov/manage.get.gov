42151d975c027d0859c5841a929aaf03866ad95f
aditi-green <aditi.green@ecstech.com>
2025-12-24 16:13:34 -0500

    updated to use script again

--- ops/scripts/write_git_info_to_env.sh
+++ ops/scripts/write_git_info_to_env.sh
@@ -1,4 +1,32 @@
 #!/bin/sh
+# Running locally,
+# Assumptions: Already logged into the cloud.gov cli
+# Notes: 
+# Script restages app at the end to update changes
+# Versioning info can be found at url: /health
+# The following has to be exported on the command line
+# export ENVIRONMENT="yourapp"
+# export BRANCH="branch-name"
+# export COMMIT="commit"
+# export TAG="tag"
+# 
+
+if [ "$GITHUB_ACTIONS" = "true"]; then
+    echo "Running in Github Actions"
+    IS_CI=true
+else
+    echo "Running locally"
+    IS_CI=false
+fi
+
+if ["$IS_CI" = true ]; then
+    echo "Installing CF CLI ...."
+    wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo gpg --dearmor -o /usr/share/keyrings/cli.cloudfoundry.org.gpg
+    echo "deb [signed-by=/usr/share/keyrings/cli.cloudfoundry.org.gpg] https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+    sudo apt-get update
+    sudo apt-get install cf8-cli
+    echo "CF CLI installed"
+fi
 
 echo "Updating git info for environment in process"
 echo " Branch: $BRANCH"
@@ -11,9 +39,20 @@ else
     APP_NAME="getgov-${ENVIRONMENT}"
 fi
 
+if ["$IS_CI" = true ]; then
+    echo "Authenticating..."
+    cf api api.fr.cloud.gov
+    cf auth "$CF_USERNAME" "$CF_PASSWORD"
+    cf target cisa-dotgov -s "$ENVIRONMENT"
+fi
 
 cf set-env $APP_NAME GIT_BRANCH "$BRANCH"
 cf set-env $APP_NAME GIT_COMMIT "$COMMIT"
 cf set-env $APP_NAME GIT_TAG "$TAG"
 
 echo "Git info Updated for $ENVIRONMENT"
+
+if [ "$IS_CI" = false ]; then
+    echo "app is restaging for changes to take effect"
+    cf restage "getgov-$ENVIRONMENT"
+fi
