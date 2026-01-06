1193858096425fe115b1a7e6e6da158e8b728d5a
Erin <121973038+erinysong@users.noreply.github.com>
2026-01-06 15:44:54 -0800

    Update MockCloudflareService cf id length

--- src/registrar/models/dns/vendor_dns_account.py
+++ src/registrar/models/dns/vendor_dns_account.py
@@ -6,6 +6,9 @@ class VendorDnsAccount(TimeStampedModel):
     dns_vendor = models.ForeignKey(
         "registrar.DnsVendor", null=False, blank=False, on_delete=models.CASCADE, related_name="vendor_accounts"
     )
-    x_account_id = models.CharField(max_length=50)  # tbd: current ids have 32 chars
+    x_account_id = models.CharField(
+        max_length=32,
+        unique=True
+    )  # tbd: current ids have 32 chars
     x_created_at = models.DateTimeField()
     x_updated_at = models.DateTimeField()
