5790157224c4489449f9493ab1ba8670a45ff0fd
Erin <121973038+erinysong@users.noreply.github.com>
2026-01-07 11:00:30 -0800

    Update nameservers default from [] to list

--- src/registrar/models/dns/dns_zone.py
+++ src/registrar/models/dns/dns_zone.py
@@ -31,7 +31,7 @@ class DnsZone(TimeStampedModel):
 
     name = models.CharField(null=True, blank=True, unique=True)
 
-    nameservers = ArrayField(models.CharField(), null=False, blank=True, default=[])
+    nameservers = ArrayField(models.CharField(), null=False, blank=True, default=list)
 
     flatten_all_cnames = models.BooleanField(default=False)
 
