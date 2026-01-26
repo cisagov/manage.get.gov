b79e4adbb7821d4a54446ecb92d8883de2e04589
CocoByte <nicolle.leclair@gmail.com>
2026-01-26 15:47:35 -0700

    fixed remaining test

--- src/registrar/tests/test_views_request.py
+++ src/registrar/tests/test_views_request.py
@@ -3537,9 +3537,10 @@ class TestDomainRequestWizard(TestWithUser, WebTest):
         # Create a domain request with city org type and election board set
         domain_request = completed_domain_request(
             status=DomainRequest.DomainRequestStatus.STARTED, 
-            user=self.user, 
-            organization_type = DomainRequest.OrganizationChoices.CITY,
-            is_election_board = True)
+            user=self.user,
+            is_election_board=True
+        )
+        domain_request.organization_type = DomainRequest.OrganizationChoices.CITY
         domain_request.save()
 
         # DO NOT add a portfolio (this test applies to legacy domain requests only)
