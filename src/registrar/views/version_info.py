247af022823870c64af43f73ab8ef71fd7a797f5
aditi-green <aditi.green@ecstech.com>
2026-02-02 10:34:42 -0500

    adjusted default values

--- src/registrar/views/version_info.py
+++ src/registrar/views/version_info.py
@@ -5,11 +5,10 @@ import os
 
 @login_not_required
 def version_info(request):
-
     context = {
-        "git_branch": os.getenv("GIT_BRANCH", ""),
-        "git_commit_hash": os.getenv("GIT_COMMIT_SHA", "Not Found"),
-        "git_tag": os.getenv("GIT_TAG", ""),
+        "git_branch": os.getenv("GIT_BRANCH"),
+        "git_commit_hash": os.getenv("GIT_COMMIT_SHA"),
+        "git_tag": os.getenv("GIT_TAG"),
     }
 
     return render(request, "version-info.html", context)
