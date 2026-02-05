1756b09c7f76b102a28108761b1aceac252fe994
aditi-green <aditi.green@ecstech.com>
2026-02-05 09:30:57 -0500

    changes to deploy script for staging

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -9,16 +9,28 @@ if [ -z "$SANDBOXNAME" ]; then
     exit 1
 fi
 
+
 # Compile and collect static assets
 ../ops/scripts/build.sh
 
 # Collect git info
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$SANDBOXNAME" == "staging" ]]; then
+    echo "HERE WE ARE"
+    TAG=$(git describe --tags --abbrev=0)
+else
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+fi
+
 COMMIT=$(git rev-parse HEAD)
 
 # Deploy to sandbox 
 cf target -o cisa-dotgov -s $1
-cf push getgov-$1 -f  ../ops/manifests/manifest-$1.yaml --var GIT_BRANCH="$BRANCH" --var GIT_COMMIT_SHA="$COMMIT" --strategy rolling 
+
+if [ "$SANDBOXNAME" = "staging"]; then   
+    cf push getgov-$1 -f  ../ops/manifests/manifest-$1.yaml --var GIT_TAG="$TAG" --var GIT_COMMIT_SHA="$COMMIT" --strategy rolling 
+else
+    cf push getgov-$1 -f  ../ops/manifests/manifest-$1.yaml --var GIT_BRANCH="$BRANCH" --var GIT_COMMIT_SHA="$COMMIT" --strategy rolling 
+fi
 
 # migrations need to be run manually. Developers can use this command
 #cf run-task getgov-SANDBOXNAME --command 'python manage.py migrate' --name migrate
