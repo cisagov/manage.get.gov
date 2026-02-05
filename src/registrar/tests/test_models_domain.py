16930640f44d2dbce464c2dc9ec16a350e7a6dc3
Charles Wells <charles.wells@ecstech.com>
2026-02-05 15:24:01 -0500

    #4376: Add created_at fields to domain model

--- src/registrar/tests/test_models_domain.py
+++ src/registrar/tests/test_models_domain.py
@@ -2645,6 +2645,23 @@ class TestCreationDate(MockEppLib):
         self.domain.statuses
         self.assertEquals(self.domain.created_at, self.creation_date)
 
+    def test_registry_created_at_and_created_at_reference(self):
+        """assert registry_created_at is set from EPP and created_at_reference is preserved"""
+        original_created_at = self.domain.created_at
+        # force fetch_cache to be called
+        self.domain.statuses
+        self.domain.refresh_from_db()
+        self.assertEquals(self.domain.registry_created_at, self.creation_date)
+        self.assertEquals(self.domain.created_at_reference, original_created_at)
+
+
+class TestDomainCreatedAtReference(TestCase):
+    def test_created_at_reference_populated_on_create(self):
+        domain = Domain.objects.create(name="created-at-ref.gov")
+        domain.refresh_from_db()
+        self.assertEqual(domain.created_at_reference, domain.created_at)
+        self.assertIsNone(domain.registry_created_at)
+
 
 class TestAnalystClientHold(MockEppLib):
     """Rule: Analysts may suspend or restore a domain by using client hold"""
