a48faddd7e3dcbbf0b6ac6b7b6f2d587919f9917
aditi-green <aditi.green@ecstech.com>
2026-02-12 10:59:41 -0500

    made suggested updates

--- src/registrar/views/domain.py
+++ src/registrar/views/domain.py
@@ -832,15 +832,15 @@ class DomainDNSRecordsView(DomainFormBaseView):
         return "DNS records"
 
     def record_dict_for_initial_data(self, dns_record):
-        # Converting model values into a dict to fill in edit form
+        """ Converting model values into a dict to fill in edit form """
         rec_dict = {}
         for field in dns_record._meta.fields:
             rec_dict[f"{field.name}"] = getattr(dns_record, field.name)
         return rec_dict
 
     def attach_edit_form(self, dns_records):
-        # adding a form instance to the dns_record objects
-        # to display corresponding values in the table rows
+       """ adding a form instance to the dns_record objects
+        to display corresponding values in the table rows """
         for record in dns_records:
             data_dict = self.record_dict_for_initial_data(record)
             record.form = DomainDNSRecordForm(initial=data_dict)
