d4cfc6fa8a899b6f23aed1cf65ddbb65bd2997b8
Abraham Alam <abraham.alam@ecstech.com>
2026-02-26 10:33:22 -0500

    final cleanup of JS file

--- src/registrar/assets/src/js/getgov/domain-dns-record-content.js
+++ src/registrar/assets/src/js/getgov/domain-dns-record-content.js
@@ -1,6 +1,5 @@
 // Establishes javascript for dynamic content label based on type
 export function initDynamicDNSRecordFormFields() { 
-    
     const typeField = document.getElementById('id_type');
 
     if (!typeField) return;
@@ -9,25 +8,25 @@ export function initDynamicDNSRecordFormFields() {
         typeField.dataset.typeConfig || "{}"
     );
 
-    // Getting and cloning the required field asterisk
     const contentLabel = document.querySelector('label[for=id_content]');
+    const contentHelp = document.getElementById('id_content_helptext');
+    
+    // Getting and cloning the required field asterisk
     const abbrElement = contentLabel?.querySelector('abbr');
     const abbrClone = abbrElement ? abbrElement.cloneNode(true) : null;
 
     typeField.addEventListener('change', function (){
         const selectedType = this.value;
         const info = config[selectedType];
-        const currentContentLabel = document.querySelector('label[for=id_content]');
-        const currentContentHelp = document.getElementById('id_content_helptext');
         
         
         if (info) {
-            currentContentLabel.textContent = info.label;
-            currentContentHelp.textContent = info.help_text;
+            contentLabel.textContent = info.label;
+            contentHelp.textContent = info.help_text;
         }
 
         // Appending the asterisk to the label
-        currentContentLabel.appendChild(abbrClone);
+        contentLabel.appendChild(abbrClone);
     });
 
     // Defensive edge case, if type is pre-selected (ex: submitting with errors)
