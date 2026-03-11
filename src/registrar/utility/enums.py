6960383f06d25058ce88f2405506e6739716aef5
aditi-green <aditi.green@ecstech.com>
2026-03-11 12:15:59 -0400

    change the wrong enum

--- src/registrar/utility/enums.py
+++ src/registrar/utility/enums.py
@@ -151,13 +151,13 @@ class DNSRecordTypes(TextChoices):
         return {
             DNSRecordTypes.A: "Enter a valid IPv4 address using numbers and periods.",
             DNSRecordTypes.AAAA: "Enter a valid IPv6 address using numbers and colons.",
-        }.get(self, None)
+        }.get(self, "")
 
     @property
     def use_textarea(self) -> bool:
         return {
             DNSRecordTypes.TXT: True,
-        }.get(self, "")
+        }.get(self, False)
 
     @property
     def get_partial(self) -> str:
