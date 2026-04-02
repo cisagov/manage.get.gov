00cb62e921262588acc28d69bd5d137d5d69cb34
asaki222 <130692870+asaki222@users.noreply.github.com>
2026-04-02 18:46:32 -0400

    #4754 - Add comment feature within dns records table row [GLACIER] (#4838)

--- src/registrar/tests/views/test_domain_dns_record_and_form.py
+++ src/registrar/tests/views/test_domain_dns_record_and_form.py
@@ -211,3 +211,22 @@ class TestDomainDNSRecordsView(TestWithDNSRecordPermissions, WebTest):
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "content-field-wrapper-txt")
+
+    @override_flag("dns_hosting", active=True)
+    @less_console_noise_decorator
+    def test_get_dns_records_page_displays_comments(self):
+        """
+        If comments are on the record, they should be available in the dns records page
+        """
+        for record_case in self.RECORD_TEST_CASES:
+            create_dns_record(
+                self.dns_zone,
+                record_type=record_case["type"],
+                record_name=record_case["name"],
+                record_content=record_case["content"],
+                record_comment=record_case["comment"],
+            )
+        response = self.client.get(self._url())
+
+        for record_case in self.RECORD_TEST_CASES:
+            self.assertContains(response, f'Toggle to view comment for {record_case["name"]}')
