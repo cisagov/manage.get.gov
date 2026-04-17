bca086bf39f63ca60967b3009562b797f0d9baa7
Kim Allen <kim@truss.works>
2026-04-17 13:39:36 -0700

    Add model level cleaning

--- src/registrar/models/dns/dns_record.py
+++ src/registrar/models/dns/dns_record.py
@@ -59,6 +59,12 @@ class DnsRecord(TimeStampedModel):
         if self.ttl < 60 or self.ttl > 86400:
             errors["ttl"] = ["TTL for unproxied records must be between 60 and 86400."]
 
+    def _clean_content(self, record_type, content):
+        """Clean content based on record type."""
+        cleaner = record_type.cleaner
+        if cleaner and self.content:
+            self.content = cleaner(self.content)
+
     def _validate_content(self, record_type, errors):
         """Validate content based on record type."""
         validator = record_type.validator
@@ -107,6 +113,7 @@ class DnsRecord(TimeStampedModel):
 
         self._validate_ttl(errors)
         record_type = DNSRecordTypes(self.type)
+        self._clean_content(record_type, self.content)  # Must clean befor validating
         self._validate_content(record_type, errors)
         self._validate_mx_priority(record_type, errors)
         self._validate_exclusive_names(record_type, errors)
