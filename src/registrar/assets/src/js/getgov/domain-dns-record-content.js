11d67f0d9146528b7727667a6ab6f900a29a8995
aditi-green <aditi.green@ecstech.com>
2026-04-23 18:29:43 -0400

    fixed counter text

--- src/registrar/assets/src/js/getgov/domain-dns-record-content.js
+++ src/registrar/assets/src/js/getgov/domain-dns-record-content.js
@@ -7,7 +7,7 @@ function getCharCountText (charLimit, charLength) {
     }
     else{
         const charactersLeft = Math.abs(charLimit - charLength);
-        const remainingText =  charLimit > charLength ? "left" : "over limit";
+        const remainingText =  charLimit < charLength ? "over limit" : "left";
         const characters =`character${charactersLeft === 1 ? '' : 's'}`;
         finalString = `${charactersLeft} ${characters} ${remainingText}`
     }
