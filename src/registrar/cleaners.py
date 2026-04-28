3ca0bcab666fd38dc0c5127f7fd3cd0d6746b0c1
Kim Allen <kim@truss.works>
2026-04-28 11:08:39 -0700

    Move clean fn into its own cleaner.py

---
+++ src/registrar/cleaners.py
@@ -0,0 +1,6 @@
+
+def clean_txt_content(content: str) -> str:
+    """Clean the content field for TXT records to ensure it is enclosed in quotes."""
+    if content and not (content.startswith('"') and content.endswith('"')):
+        content = f'"{content}"'
+    return content
--- src/registrar/validations.py
+++ src/registrar/validations.py
@@ -230,11 +230,3 @@ def validate_mx_content(content: str) -> None:
 
     if len(content) > MX_CONTENT_MAX_LENGTH:
         raise ValidationError("Name must be no more than 253 characters.")
-
-
-# Add this here temporarily, consider creating a cleaners.py file
-def clean_txt_content(content: str) -> str:
-    """Clean the content field for TXT records to ensure it is enclosed in quotes."""
-    if content and not (content.startswith('"') and content.endswith('"')):
-        content = f'"{content}"'
-    return content
