bd85196b203ce5e2fe90a73f810270225ac8c45c
Kim Allen <kim@truss.works>
2026-05-18 21:38:17 -0700

    #4860: TXT DNS record validations update (#5013)

--- src/registrar/validations.py
+++ src/registrar/validations.py
@@ -78,6 +78,8 @@ CNAME_NAME_TARGET_BANNER_ERROR_MESSAGE = "Name and target can't be the same."
 CNAME_NAME_INLINE_ERROR_MESSAGE = "Name can't be the same as the target."
 CNAME_TARGET_INLINE_ERROR_MESSAGE = "Target can't be the same as the record name."
 MX_CONTENT_SPACES_ERROR_MESSAGE = "Enter the mail server without any spaces."
+TXT_RECORD_CONTENT_QUOTES_ERROR_MESSAGE = "Enter content using quotation marks at neither the beginning nor end."
+TXT_RECORD_CONTENT_MAX_LENGTH_ERROR_MESSAGE = "Content must be no more than 4080 characters."
 
 
 def _validate_dns_name_structure(name: str) -> None:
@@ -188,26 +190,22 @@ def validate_dns_name_fqdn_length(name: str, zone_name: str | None) -> None:
         raise ValidationError(DNS_NAME_LENGTH_ERROR_MESSAGE)
 
 
-def check_has_valid_quotes(content: str) -> bool:
+def check_has_invalid_quoted_string(content: str) -> bool:
     double_quote = '"'
-    quote_count = content.count(double_quote)
 
-    # check if string begins and ends with a quote or no quote at all
+    # check if string begins or ends with a quote
     first_item_char_is_double_quote = content[0] == double_quote
     last_item_is_double_quote = content[len(content) - 1] == double_quote
 
-    return quote_count % 2 != 0 or first_item_char_is_double_quote != last_item_is_double_quote
+    return first_item_char_is_double_quote or last_item_is_double_quote
 
 
 def validate_txt_content(content: str) -> None:
+    if check_has_invalid_quoted_string(content):
+        raise ValidationError(TXT_RECORD_CONTENT_QUOTES_ERROR_MESSAGE)
 
-    if check_has_valid_quotes(content):
-        raise ValidationError(
-            'Record content is not quoted correctly; ensure it begins and ends with double quotes(").'
-        )
-
-    if len(content) > 2048:
-        raise ValidationError("Content must be no more than 2048 characters.")
+    if len(content) > 4080:
+        raise ValidationError(TXT_RECORD_CONTENT_MAX_LENGTH_ERROR_MESSAGE)
 
 
 def validate_mx_content(content: str) -> None:
