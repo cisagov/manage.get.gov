6f8283a5074cd39bf6f281d81ae40f2da3dab0b8
CuriousX <nicolle.leclair@gmail.com>
2026-08-02 18:20:26 -0400

    #4887 Remediate case sensitive queries (iexact patch) (#5210)

--- src/registrar/views/portfolios.py
+++ src/registrar/views/portfolios.py
@@ -878,7 +878,7 @@ class PortfolioInvitedMemberDomainsEditView(DetailView, View):
                 messages.warning(self.request, "Could not send email notification to existing domain managers.")
 
             # Update existing invitations from CANCELED to INVITED
-            existing_invitations = DomainInvitation.objects.filter(domain__in=added_domains, email=email)
+            existing_invitations = DomainInvitation.objects.filter(domain__in=added_domains, email__iexact=email)
             existing_invitations.update(status=DomainInvitation.DomainInvitationStatus.INVITED)
 
             # Determine which domains need new invitations
@@ -936,7 +936,7 @@ class PortfolioInvitedMemberDomainsEditView(DetailView, View):
         # Update invitations from INVITED to CANCELED
         DomainInvitation.objects.filter(
             domain_id__in=removed_domain_ids,
-            email=email,
+            email__iexact=email,
             status=DomainInvitation.DomainInvitationStatus.INVITED,
         ).update(status=DomainInvitation.DomainInvitationStatus.CANCELED)
 
