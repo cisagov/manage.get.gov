a46c5a6e3193c72f11e0860d98a549043df19fec
Erin <121973038+erinysong@users.noreply.github.com>
2026-08-19 16:03:10 -0700

    Grab cf account id before db deletion

--- src/registrar/models/domain.py
+++ src/registrar/models/domain.py
@@ -1326,6 +1326,7 @@ class Domain(TimeStampedModel, DomainHelper):
                     logger.info("Removing db DNS account data for %s.", self.name)
                     dns_account = dns_zone.dns_account
                     vendor_account = DnsAccount_VendorDnsAccount.objects.get(dns_account=dns_account).vendor_dns_account
+                    x_account_id = vendor_account.x_account_id
                     dns_account.delete()
                     vendor_account.delete()
                     logger.info("Removed db DNS account data for domain %s.", self.name)
@@ -1334,7 +1335,7 @@ class Domain(TimeStampedModel, DomainHelper):
                     from registrar.services.dns_host_service import DnsHostService
 
                     dns_host_service = DnsHostService()
-                    dns_host_service.delete_account(vendor_account.x_account_id)
+                    dns_host_service.delete_account(x_account_id)
             except Exception as e:
                 logger.error("Error deleting DNS data for %s: %s", self.name, e, exc_info=True)
                 raise e
