c13f4604b47f4703795a0e41d213d8ed0562e570
aditi-green <aditi.green@ecstech.com>
2026-08-20 13:30:24 -0400

    made it DRY

--- src/registrar/validations.py
+++ src/registrar/validations.py
@@ -101,10 +101,8 @@ def get_trailing_number_message_by_record_type(field_type):
         field_type = "content"
 
     requirement = requirement_dict.get(field_type, trailing_number_error_message)
-    if field_type == "content":
-        return f"Enter {field_type} {requirement}."
-    else:
-        return f"Enter a {field_type} {requirement}"
+ 
+    return f"Enter {'a' if field_type != "content" else ''} {field_type} {requirement}"
 
 
 # For system level validation
