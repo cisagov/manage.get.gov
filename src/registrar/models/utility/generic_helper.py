453b4475a875419566430f4a64110357033b7823
Samir Mishra <samir.mishra@ecstech.com>
2026-08-27 11:27:27 -0400

    Added some logging

--- src/registrar/models/utility/generic_helper.py
+++ src/registrar/models/utility/generic_helper.py
@@ -333,7 +333,7 @@ def get_url_name(path):
         return match.url_name
     except Resolver404:
         try:
-            logger.error(f"Path {path} not resolved. Rechecking with a trailing slash")
+            logger.info(f"Path {path} not resolved. Rechecking with a trailing slash")
             match = resolve(path + "/")
             return match.url_name
         except Resolver404:
