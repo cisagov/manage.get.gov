a7ffcd6bac1c13e56cef7992ea4936e7dd7662e2
Kim Allen <kim@truss.works>
2026-09-02 12:39:02 -0700

    Move initialization of MockCloudflareService

--- src/registrar/apps.py
+++ src/registrar/apps.py
@@ -12,8 +12,3 @@ class RegistrarConfig(AppConfig):
 
         from . import checks  # noqa: F401  # imported to register system checks, flake8 can ignore 'unused import'
 
-        if settings.DNS_MOCK_EXTERNAL_APIS:
-            from registrar.services.mock_cloudflare_service import MockCloudflareService
-
-            mock_cloudflare_service = MockCloudflareService()
-            mock_cloudflare_service.start()
