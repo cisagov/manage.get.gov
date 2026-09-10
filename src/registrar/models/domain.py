a44306569ad1e857f27fc89134f922d3083306cd
aditi-green <aditi.green@ecstech.com>
2026-09-10 17:33:47 -0400

    updated with new conditionals

--- src/registrar/models/domain.py
+++ src/registrar/models/domain.py
@@ -1540,7 +1540,7 @@ class Domain(TimeStampedModel, DomainHelper):
     def enrolled_hosting_display(self, request=None):
         return "Yes" if self.is_enrolled_in_dns_hosting else "No"
 
-    def is_using_external_hosting(self,request):
+    def is_using_external_hosting(self,request=None):
         if not flag_is_active_for_user(request, "dns_hosting") and self.state in [self.State.READY, self.State.ON_HOLD]:
             return True
         else:
