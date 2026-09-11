765f57421f6a44b526c626200edc9cb709855f91
Abraham Alam <abraham.alam@ecstech.com>
2026-09-11 14:12:02 -0400

    form switch fix for text preview to appear after modal v1

--- src/registrar/assets/src/js/getgov/domain-dns-form-switcher.js
+++ src/registrar/assets/src/js/getgov/domain-dns-form-switcher.js
@@ -147,7 +147,7 @@ export class RecordSelectTypeSwitcher extends DNSFormSwitcher{
         const selectEl= this.container.querySelector("#id_type")
         selectEl.selectedIndex = value;
         // programmatically dispatches change event for alpine to switch the form, and to get the appropiate form labels
-        const changeEvent = new Event('change');
+        const changeEvent = new Event('change', {bubbles: true});
         selectEl.dispatchEvent(changeEvent);
     }
 
