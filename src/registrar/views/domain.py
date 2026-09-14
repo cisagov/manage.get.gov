e1f048bf48325c1ad2851f5174b7f62b3457b4ff
aditi-green <aditi.green@ecstech.com>
2026-09-14 11:42:27 -0400

    remove the comment

--- src/registrar/views/domain.py
+++ src/registrar/views/domain.py
@@ -1288,7 +1288,6 @@ class DomainNameserversView(DomainFormBaseView):
             messages.error(self.request, NameserverError(code=nsErrorCodes.BAD_DATA))
             logger.error(f"Nameservers error: {Err}")
         # TODO: registry is not throwing an error when no connection
-        # OTHER REGISTRY ERROR
         except RegistryError as Err:
             if Err.is_connection_error():
                 messages.error(
