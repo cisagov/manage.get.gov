78b30b54b0fc1dca9f7f59a2d726f88cb37013d4
Kim Allen <kim@truss.works>
2026-09-21 14:03:31 -0700

    Add modified get method from delete_cf_acounts

--- src/registrar/services/cloudflare_service.py
+++ src/registrar/services/cloudflare_service.py
@@ -1,7 +1,7 @@
 from contextlib import contextmanager
 
 from httpx import RequestError, HTTPStatusError, HTTPError
-from typing import Any
+from typing import Any, List
 import logging
 from dataclasses import dataclass
 from django.conf import settings
@@ -266,11 +266,44 @@ class CloudflareService:
 
             return resp.json()
 
-    def get_tenant_accounts(self):
-        accounts = []
-        # GET list accounts by page until all pages are gotten
+    def get_tenant_accounts(self, per_page: int = 50) -> List[dict]:
+            """Fetch all accounts under the tenant with pagination"""
+            all_accounts = []
+            page = 1
+            is_last_page = False
 
-        return accounts
+            while True:
+                appended_url = f"/tenants/{self.tenant_id}/accounts"
+                params = {"page": page, "per_page": per_page}
+
+                with self._dns_call():
+                    logger.info(
+                        "Looking up tenant accounts by page: %s",
+                        page,
+                    )
+                    resp = self.client.get(appended_url, params=params)
+                    resp.raise_for_status()
+
+                accounts_data = resp.json()
+
+                accounts = accounts_data.get("result", [])
+
+                if not accounts:
+                    # No more accounts to fetch
+                    break
+
+                all_accounts.extend(accounts)
+
+                # Check if there are more pages
+                result_info = accounts_data.get("result_info", {})
+                total_count = result_info.get("total_count", 1)
+                is_last_page = total_count <= page * per_page
+                if is_last_page:
+                    break
+
+                page += 1
+
+            return all_accounts
 
     def get_zone_by_id(self, x_zone_id: str):
         """Get zone data given a Clouflare zone id"""
